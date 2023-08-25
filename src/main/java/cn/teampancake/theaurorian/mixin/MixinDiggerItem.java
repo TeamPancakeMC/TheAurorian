f998cca75ea4871fcfbbcf852bc247460361b0be
glyceryl <494192175@qq.com>
2023-08-26 00:02:11 +0800

    Fix a Mixin bug in about DiggerItem.

--- src/main/java/cn/teampancake/theaurorian/mixin/MixinDiggerItem.java
+++ src/main/java/cn/teampancake/theaurorian/mixin/MixinDiggerItem.java
@@ -1,12 +1,13 @@
 package cn.teampancake.theaurorian.mixin;
 
-import cn.teampancake.theaurorian.common.items.ModArmorMaterials;
+import cn.teampancake.theaurorian.common.items.ModToolTiers;
 import cn.teampancake.theaurorian.utils.AurorianSteelHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,11 +16,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DiggerItem.class)
-public class MixinDiggerItem {
+public class MixinDiggerItem extends TieredItem {
+
+    public MixinDiggerItem(Tier tier, Properties properties) {
+        super(tier, properties);
+    }
 
     @Inject(method = "mineBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Ljava/util/function/Consumer;)V", shift = At.Shift.AFTER))
     public void mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
-        if (stack.getItem() instanceof ArmorItem armorItem && armorItem.getMaterial() == ModArmorMaterials.AURORIAN_STEEL) {
+        if (this.getTier() == ModToolTiers.AURORIAN_STEEL) {
             AurorianSteelHelper.handleAurorianSteelDurability(stack);
         }
     }

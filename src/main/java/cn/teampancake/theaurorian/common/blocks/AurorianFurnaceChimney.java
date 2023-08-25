49996eabc9e5e8cc67656bb0488b643d02145df6
glyceryl <494192175@qq.com>
2023-08-25 12:56:29 +0800

    Fix chimney tooltips bug.

--- src/main/java/cn/teampancake/theaurorian/common/blocks/AurorianFurnaceChimney.java
+++ src/main/java/cn/teampancake/theaurorian/common/blocks/AurorianFurnaceChimney.java
@@ -1,26 +1,18 @@
 package cn.teampancake.theaurorian.common.blocks;
 
-import cn.teampancake.theaurorian.config.AurorianConfig;
-import net.minecraft.ChatFormatting;
-import net.minecraft.client.gui.screens.Screen;
+import cn.teampancake.theaurorian.common.items.ITooltipsItem;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 @SuppressWarnings("deprecation")
-public class AurorianFurnaceChimney extends Block {
+public class AurorianFurnaceChimney extends Block implements ITooltipsItem {
 
     public AurorianFurnaceChimney(Properties properties) {
         super(properties);
@@ -38,16 +30,6 @@ public class AurorianFurnaceChimney extends Block {
             double d1 = (double) pos.getY() + 0.5D + (random.nextDouble() * 6.0D / 16.0D);
             double d2 = (double) pos.getZ() + 0.5D;
             level.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-        }
-    }
-
-    @Override
-    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
-        if (!Screen.hasShiftDown()) {
-            tooltip.add(Component.translatable("string.theaurorian.tooltip.shiftinfo").withStyle(ChatFormatting.ITALIC));
-        } else {
-            int maximumChimneys = AurorianConfig.CONFIG_MAXIMUM_CHIMNEYS.get();
-            tooltip.add(Component.translatable("string.theaurorian.tooltip.furnacechimney", maximumChimneys));
         }
     }
 

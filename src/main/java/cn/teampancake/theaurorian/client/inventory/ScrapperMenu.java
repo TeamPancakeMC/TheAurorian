5c03f86e07ba8144aeadc1611ce044dfe201ee9d
mlus-Asuka <1319237806@qq.com>
2023-09-08 20:15:14 +0800

    adjust quickMoveStack for Scrapper

--- src/main/java/cn/teampancake/theaurorian/client/inventory/ScrapperMenu.java
+++ src/main/java/cn/teampancake/theaurorian/client/inventory/ScrapperMenu.java
@@ -5,10 +5,13 @@ import cn.teampancake.theaurorian.registry.ModItems;
 import cn.teampancake.theaurorian.registry.ModMenuTypes;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import org.jetbrains.annotations.NotNull;
 
 public class ScrapperMenu extends AbstractSimpleMenu {
 
@@ -42,6 +45,55 @@ public class ScrapperMenu extends AbstractSimpleMenu {
     @Override
     protected boolean slot2ItemFlag(ItemStack stack) {
         return stack.is(ModItems.CRYSTAL.get());
+    }
+
+    @Override
+    public @NotNull ItemStack quickMoveStack(@NotNull Player player, int index) {
+        ItemStack prevItem = ItemStack.EMPTY;
+        Slot slot = this.slots.get(index);
+        if (slot.hasItem()) {
+            ItemStack slotItem = slot.getItem();
+            prevItem = slotItem.copy();
+            if (index == 2 ) {
+                if (!this.moveItemStackTo(slotItem, 3, 39, true)) {
+                    return ItemStack.EMPTY;
+                }
+                slot.onQuickCraft(slotItem, prevItem);
+            } else if(index != 0 && index != 1){
+                if (slot1ItemFlag(slotItem)) {
+                    if (!this.moveItemStackTo(slotItem, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (slot2ItemFlag(slotItem)) {
+                    if (!this.moveItemStackTo(slotItem, 1, 2, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }else if (index >= 3 && index < 30) {
+                    if (!this.moveItemStackTo(slotItem, 30, 39, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (index >= 30 && index < 39 && !this.moveItemStackTo(slotItem, 3, 30, false)) {
+                    return ItemStack.EMPTY;
+                }else if (!this.moveItemStackTo(slotItem, 0, 2, true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.moveItemStackTo(slotItem, 3, 39, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (slotItem.isEmpty()) {
+                slot.set(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+
+            if (slotItem.getCount() == prevItem.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(player, slotItem);
+        }
+        return prevItem;
     }
 
 }

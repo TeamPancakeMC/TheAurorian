69a11eca5a39eb68d4290819589a5d931e76e520
glyceryl <494192175@qq.com>
2023-09-08 15:02:13 +0800

    Optimize the code of "MoonlightForgeBlockEntity"

--- src/main/java/cn/teampancake/theaurorian/common/blocks/entity/MoonlightForgeBlockEntity.java
+++ src/main/java/cn/teampancake/theaurorian/common/blocks/entity/MoonlightForgeBlockEntity.java
@@ -1,34 +1,19 @@
 package cn.teampancake.theaurorian.common.blocks.entity;
 
+import cn.teampancake.theaurorian.client.inventory.MoonlightForgeMenu;
 import cn.teampancake.theaurorian.registry.ModBlockEntityTypes;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.core.NonNullList;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.Container;
-import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class MoonlightForgeBlockEntity extends BaseContainerBlockEntity {
+public class MoonlightForgeBlockEntity extends SimpleContainerBlockEntity {
 
-    private LazyOptional<IItemHandler> lazyOptional = LazyOptional.empty();
-    private final MoonlightForgeBlockEntity.Handler handler = new Handler();
     private boolean hasMoonLight;
     private boolean isCrafting;
     private boolean isPowered;
@@ -98,11 +83,6 @@ public class MoonlightForgeBlockEntity extends BaseContainerBlockEntity {
     }
 
     private void doCraft() {
-        ItemStack output = null;
-        ItemStack input1 = this.handler.getStackInSlot(0);
-        ItemStack input2 = this.handler.getStackInSlot(1);
-        ItemStack req1 = null;
-        ItemStack req2 = null;
         //Todo: Should finish the migration of recipes.
     }
 
@@ -120,109 +100,14 @@ public class MoonlightForgeBlockEntity extends BaseContainerBlockEntity {
         }
     }
 
-    @NotNull @Override
-    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-        if (side != null && cap == ForgeCapabilities.ITEM_HANDLER) {
-            return this.lazyOptional.cast();
-        }
-        return super.getCapability(cap, side);
-    }
-
-    @Override
-    public void onLoad() {
-        super.onLoad();
-        lazyOptional = LazyOptional.of(() -> this.handler);
-    }
-
-    @Override
-    public void invalidateCaps() {
-        super.invalidateCaps();
-        this.lazyOptional.invalidate();
-    }
-
-    @Override
-    protected void saveAdditional(CompoundTag tag) {
-        tag.put("inventory", this.handler.serializeNBT());
-        super.saveAdditional(tag);
-    }
-
-    @Override
-    public void load(CompoundTag tag) {
-        super.load(tag);
-        this.handler.deserializeNBT(tag);
-    }
-
     @Override
     protected Component getDefaultName() {
         return Component.translatable("theaurorian.container.moonlight_forge");
     }
 
     @Override
-    protected AbstractContainerMenu createMenu(int pContainerId, Inventory pInventory) {
-        return null;
-    }
-
-    @Override
-    public int getContainerSize() {
-        return this.handler.getSlots();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        for (ItemStack stack : this.handler.getStacks()) {
-            if (!stack.isEmpty()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    @Override
-    public ItemStack getItem(int slot) {
-        return this.handler.getStackInSlot(slot);
-    }
-
-    @Override
-    public ItemStack removeItem(int pIndex, int pCount) {
-        return ContainerHelper.removeItem(this.handler.getStacks(), pIndex, pCount);
-    }
-
-    @Override
-    public ItemStack removeItemNoUpdate(int slot) {
-        return ContainerHelper.takeItem(this.handler.getStacks(), slot);
-    }
-
-    @Override
-    public void setItem(int pSlot, ItemStack pStack) {
-
-    }
-
-    @Override
-    public boolean stillValid(Player player) {
-        return Container.stillValidBlockEntity(this, player);
-    }
-
-    @Override
-    public void clearContent() {
-        this.handler.getStacks().clear();
-    }
-
-    private class Handler extends ItemStackHandler {
-
-        public Handler() {
-            super(3);
-        }
-
-        @Override
-        protected void onContentsChanged(int slot) {
-            setChanged();
-        }
-
-        public NonNullList<ItemStack> getStacks() {
-            return this.stacks;
-        }
-
+    protected AbstractContainerMenu createMenu(int containerId, Inventory inventory) {
+        return new MoonlightForgeMenu(containerId, inventory, this);
     }
 
 }

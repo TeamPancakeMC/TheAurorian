cc5b0b7e509e751100faeafb4a8f390e8e016f30
glyceryl <494192175@qq.com>
2023-09-10 14:13:25 +0800

    Add flammable blocks.

--- src/main/java/cn/teampancake/theaurorian/compat/VanillaBlockCompat.java
+++ src/main/java/cn/teampancake/theaurorian/compat/VanillaBlockCompat.java
@@ -1,6 +1,9 @@
 package cn.teampancake.theaurorian.compat;
 
 import cn.teampancake.theaurorian.AurorianMod;
+import cn.teampancake.theaurorian.registry.ModBlocks;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FireBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,7 +22,20 @@ public class VanillaBlockCompat {
 
     @SubscribeEvent
     public static void registerFlammable(FMLCommonSetupEvent event) {
-
+        FireBlock fireBlock = (FireBlock)Blocks.FIRE;
+        fireBlock.setFlammable(ModBlocks.SILENT_TREE_PLANKS.get(), 5, 20);
+        fireBlock.setFlammable(ModBlocks.SILENT_WOOD_STAIRS.get(), 5, 20);
+        fireBlock.setFlammable(ModBlocks.SILENT_TREE_LOG.get(), 5, 5);
+        fireBlock.setFlammable(ModBlocks.SILENT_TREE_WOOD.get(), 5, 5);
+        fireBlock.setFlammable(ModBlocks.SILENT_TREE_LEAVES.get(), 30, 60);
+        fireBlock.setFlammable(ModBlocks.WEEPING_WILLOW_PLANKS.get(), 5, 20);
+        fireBlock.setFlammable(ModBlocks.WEEPING_WILLOW_STAIRS.get(), 5, 20);
+        fireBlock.setFlammable(ModBlocks.WEEPING_WILLOW_LOG.get(), 5, 5);
+        fireBlock.setFlammable(ModBlocks.WEEPING_WILLOW_WOOD.get(), 5, 5);
+        fireBlock.setFlammable(ModBlocks.WEEPING_WILLOW_LEAVES.get(), 30, 60);
+        fireBlock.setFlammable(ModBlocks.AURORIAN_GRASS.get(), 60, 100);
+        fireBlock.setFlammable(ModBlocks.AURORIAN_GRASS_LIGHT.get(), 60, 100);
+        fireBlock.setFlammable(ModBlocks.PETUNIA_PLANT.get(), 60, 100);
     }
 
 }

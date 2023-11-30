72575c3e7a1a7be3ec07454bad3073e0a5a592bc
glyceryl <494192175@qq.com>
2023-11-30 16:01:28 +0800

    Disable generate cache files and ".cache" directory when use DataGen.

--- src/main/java/cn/teampancake/theaurorian/mixin/MixinDataGenerator.java
+++ src/main/java/cn/teampancake/theaurorian/mixin/MixinDataGenerator.java
@@ -6,10 +6,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 @Mixin(DataGenerator.class)
 public class MixinDataGenerator {
 
     @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/data/HashCache;purgeStaleAndWrite()V"))
-    public void run(HashCache hashCache) {}
+    public void run(HashCache hashCache) throws IOException {
+        hashCache.caches.clear();
+        Files.deleteIfExists(hashCache.cacheDir);
+    }
 
 }

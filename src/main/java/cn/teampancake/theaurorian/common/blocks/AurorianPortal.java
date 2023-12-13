1d9bf2fe859771258950bb65db6becc7cb2ac48e
glyceryl <494192175@qq.com>
2023-12-13 15:39:36 +0800

    Remove the traditional portal due to we have a cool conceive about portal.

--- src/main/java/cn/teampancake/theaurorian/common/blocks/AurorianPortal.java
+++ src/main/java/cn/teampancake/theaurorian/common/blocks/AurorianPortal.java
@@ -1,14 +1,8 @@
 package cn.teampancake.theaurorian.common.blocks;
 
-import cn.teampancake.theaurorian.common.level.portal.AurorianPortalForcer;
-import cn.teampancake.theaurorian.registry.TADimensions;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.NetherPortalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -20,18 +14,5 @@ public class AurorianPortal extends NetherPortalBlock {
 
     @Override
     public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {}
-
-    @Override
-    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
-        if (entity.canChangeDimensions() && entity.level() instanceof ServerLevel serverLevel) {
-            ResourceKey<Level> aurorianKey = TADimensions.AURORIAN_DIMENSION;
-            ResourceKey<Level> resourceKey = entity.level().dimension() == aurorianKey ? Level.OVERWORLD : aurorianKey;
-            ServerLevel newServerLevel = serverLevel.getServer().getLevel(resourceKey);
-            if (newServerLevel != null && entity instanceof Player && !entity.isPassenger()) {
-                entity.setPortalCooldown();
-                entity.changeDimension(newServerLevel, new AurorianPortalForcer(newServerLevel, entity.getOnPos()));
-            }
-        }
-    }
 
 }

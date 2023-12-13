e9a573452b55626d786e266502f0eb379c673482
glyceryl <494192175@qq.com>
2023-12-13 20:45:53 +0800

    Use event to add fuel instead of mixin.

--- src/main/java/cn/teampancake/theaurorian/compat/VanillaItemCompat.java
+++ src/main/java/cn/teampancake/theaurorian/compat/VanillaItemCompat.java
@@ -3,22 +3,42 @@ package cn.teampancake.theaurorian.compat;
 import cn.teampancake.theaurorian.AurorianMod;
 import cn.teampancake.theaurorian.common.items.CeruleanArrow;
 import cn.teampancake.theaurorian.common.items.CrystalArrow;
+import cn.teampancake.theaurorian.registry.TABlocks;
 import cn.teampancake.theaurorian.registry.TAItems;
 import cn.teampancake.theaurorian.utils.TACommonUtils;
+import com.google.common.collect.Maps;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
+import java.util.Map;
+
 @SuppressWarnings("SpellCheckingInspection")
 @Mod.EventBusSubscriber(modid = AurorianMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class VanillaItemCompat {
+
+    @SubscribeEvent
+    public static void registerFurnaceFuels(FurnaceFuelBurnTimeEvent event) {
+        Map<Item, Integer> map = Maps.newLinkedHashMap();
+        map.put(TABlocks.SILENT_WOOD_CRAFTING_TABLE.get().asItem(), 300);
+        map.put(TABlocks.SILENT_WOOD_LADDER.get().asItem(), 300);
+        map.put(TAItems.SILENT_WOOD_STICK.get(), 100);
+        map.put(TAItems.SILENT_WOOD_BOW.get(), 300);
+        map.put(TAItems.AURORIAN_COAL.get(), 1500);
+        map.forEach((item, integer) -> {
+            if (event.getItemStack().getItem() == item) {
+                event.setBurnTime(integer);
+            }
+        });
+    }
 
     @SubscribeEvent
     public static void registerCompostables(FMLCommonSetupEvent event) {

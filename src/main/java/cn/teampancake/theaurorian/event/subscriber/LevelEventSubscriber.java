4b33ad0d0c8ece230bdfb29585791d27d0f08dd4
glyceryl <494192175@qq.com>
2023-12-16 16:03:51 +0800

    Remove the red sky.

--- src/main/java/cn/teampancake/theaurorian/event/subscriber/LevelEventSubscriber.java
+++ src/main/java/cn/teampancake/theaurorian/event/subscriber/LevelEventSubscriber.java
@@ -10,7 +10,7 @@ public class LevelEventSubscriber {
 
     @SubscribeEvent
     public static void registerAurorianSkyColor(RegisterAurorianSkyColorEvent event) {
-        event.register(AurorianMod.prefix("ta_red"), 0xf10029);
+
     }
 
 }

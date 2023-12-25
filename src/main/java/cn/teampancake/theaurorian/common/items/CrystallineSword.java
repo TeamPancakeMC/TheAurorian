a680a9726ad9a1bdf794d751ae19589e2e9d405f
XiaoHuNao <shiwenbin2250@gmail.com>
2023-12-25 13:32:45 +0800

    fix CrystallineSword

--- src/main/java/cn/teampancake/theaurorian/common/items/CrystallineSword.java
+++ src/main/java/cn/teampancake/theaurorian/common/items/CrystallineSword.java
@@ -1,8 +1,12 @@
 package cn.teampancake.theaurorian.common.items;
 
 import cn.teampancake.theaurorian.common.registry.TAEntityTypes;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
@@ -14,6 +18,8 @@ import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.Random;
+
 public class CrystallineSword extends SwordItem implements ITooltipsItem {
 
     public CrystallineSword() {
@@ -21,28 +27,36 @@ public class CrystallineSword extends SwordItem implements ITooltipsItem {
     }
 
     @Override
-    public void releaseUsing(ItemStack stack, Level level, LivingEntity livingEntity, int timeCharged) {
-        if (livingEntity instanceof Player player) {
-            Vec3 lookAngle = player.getLookAngle();
-            float pitch = 1.0F / (level.random.nextFloat() * 0.4F + 1.2F) + 0.5F;
-            level.playSound(null, player.getOnPos(), SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.PLAYERS, 1.0F, pitch);
-            if (this.getUseDuration(stack) - timeCharged >= 20 && !level.isClientSide) {
-                stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(player.getUsedItemHand()));
-                Arrow arrow = TAEntityTypes.CRYSTALLINE_BEAM.get().create(level);
-                if (arrow != null) {
-                    arrow.shoot(lookAngle.x, lookAngle.y, lookAngle.z,3.0F, 1.0F);
-                    arrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
-                    arrow.setNoGravity(true);
-                    arrow.setKnockback(1);
-                    level.addFreshEntity(arrow);
-                }
-            }
-        }
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
+        pPlayer.startUsingItem(pUsedHand);
+        return InteractionResultHolder.consume(itemstack);
     }
 
     @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
+        if (livingEntity instanceof Player player){
+            Vec3 lookAngle = player.getLookAngle();
+            float pitch = 1.0F / (level.random.nextFloat() * 0.4F + 1.2F) + 0.5F;
+            level.playSound(null, player.getOnPos(), SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.PLAYERS, 1.0F, pitch);
+            stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(player.getUsedItemHand()));
+            Arrow arrow = TAEntityTypes.CRYSTALLINE_BEAM.get().create(level);
+            if (arrow != null) {
+                arrow.setPos(player.getX(), player.getY() + 1.5, player.getZ());
+                arrow.shoot(lookAngle.x, lookAngle.y, lookAngle.z,3.0F, 1.0F);
+                arrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+                arrow.setNoGravity(true);
+                arrow.setKnockback(1);
+                level.addFreshEntity(arrow);
+            }
+        }
+        return stack;
+    }
+
+
+    @Override
     public int getUseDuration(ItemStack pStack) {
-        return 72000;
+        return 20 * 2;
     }
 
     @Override

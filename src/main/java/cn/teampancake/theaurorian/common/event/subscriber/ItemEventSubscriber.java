a4e538e500d2ab1a9f86343e1ccb9a8fef1f1ce1
glyceryl <494192175@qq.com>
2023-12-26 00:44:28 +0800

    Accomplish optimize of the tooltips.

--- src/main/java/cn/teampancake/theaurorian/common/event/subscriber/ItemEventSubscriber.java
+++ src/main/java/cn/teampancake/theaurorian/common/event/subscriber/ItemEventSubscriber.java
@@ -8,9 +8,7 @@ import cn.teampancake.theaurorian.common.items.TAArmorMaterials;
 import cn.teampancake.theaurorian.common.registry.TAItems;
 import cn.teampancake.theaurorian.common.utils.AurorianSteelHelper;
 import cn.teampancake.theaurorian.common.utils.AurorianUtil;
-import cn.teampancake.theaurorian.common.utils.TATooltipRenderUtils;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
@@ -19,9 +17,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -37,134 +32,11 @@ import java.util.List;
 @SuppressWarnings("ConstantConditions")
 public class ItemEventSubscriber {
 
-    private static final ResourceLocation UNCOMMON = AurorianMod.prefix("textures/gui/tooltips/uncommon.png");
-    private static final ResourceLocation RARE = AurorianMod.prefix("textures/gui/tooltips/rare.png");
-    private static final ResourceLocation EPIC = AurorianMod.prefix("textures/gui/tooltips/epic.png");
-    private static final ResourceLocation LEGENDARY = AurorianMod.prefix("textures/gui/tooltips/legendary.png");
-    private static final ResourceLocation MYTHICAL = AurorianMod.prefix("textures/gui/tooltips/mythical.png");
-    private static final ResourceLocation SLEEPING_BLACK_TEA = AurorianMod.prefix("textures/gui/tooltips/sleeping_black_tea.png");
-    private static final ResourceLocation WHITE_CHOCOLATE = AurorianMod.prefix("textures/gui/tooltips/white_chocolate.png");
-    private static final ResourceLocation RED_BOOK = AurorianMod.prefix("textures/gui/tooltips/red_book.png");
-    private static final ResourceLocation STAR_OCEAN_CROSSBOW = AurorianMod.prefix("textures/gui/tooltips/star_ocean_crossbow.png");
-    private static final ResourceLocation DREAM_DYEING_CRYSTAL_FRAGMENT = AurorianMod.prefix("textures/gui/tooltips/dream_dyeing_crystal_fragment.png");
-    private static final ResourceLocation CAT_BELL = AurorianMod.prefix("textures/gui/tooltips/cat_bell.png");
-    private static final ResourceLocation TSLAT_SWORD = AurorianMod.prefix("textures/gui/tooltips/tslat_sword.png");
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public static void onRenderTooltips(RenderTooltipEvent.Pre event) {
-        ItemStack itemStack = event.getItemStack();
-        if (itemStack.is(TAItemTags.HAS_CUSTOM_TOOLTIPS)) {
-            event.setCanceled(true);
-            if (itemStack.is(TAItemTags.IS_UNCOMMON)) {
-                int[] xOffset = new int[] {-5, -5, -3, -3};
-                int[] yOffset = new int[] {-17, -15, -17, -15, -18};
-                int[] uOffset = new int[] {0, 0, 8, 8, 16};
-                int[] vOffset = new int[] {0, 8, 0, 8, 0};
-                int[] uWidth = new int[] {8, 8, 8, 8, 12};
-                int[] vHeight = new int[] {8, 8, 8, 8, 6};
-                TATooltipRenderUtils.renderTooltips(event, UNCOMMON, (0xff686f99), (0xfff1f2ff),
-                        (0xff686f99), (48), (32), (2), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItemTags.IS_RARE)) {
-                int[] xOffset = new int[] {-5, -5, -4, -4};
-                int[] yOffset = new int[] {-17, -16, -17, -16, -20};
-                int[] uOffset = new int[] {0, 0, 9, 9, 18};
-                int[] vOffset = new int[] {0, 9, 0, 9, 0};
-                int[] uWidth = new int[] {9, 9, 9, 9, 12};
-                int[] vHeight = new int[] {9, 9, 9, 9, 10};
-                TATooltipRenderUtils.renderTooltips(event, RARE, (0xff686f99), (0xfff1f2ff),
-                        (0xff686f99), (48), (32), (2), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItemTags.IS_EPIC)) {
-                int[] xOffset = new int[] {-5, -5, -4, -4};
-                int[] yOffset = new int[] {-17, -16, -17, -16, -21};
-                int[] uOffset = new int[] {0, 0, 9, 9, 18};
-                int[] vOffset = new int[] {0, 9, 0, 9, 0};
-                int[] uWidth = new int[] {9, 9, 9, 9, 18};
-                int[] vHeight = new int[] {9, 9, 9, 9, 11};
-                TATooltipRenderUtils.renderTooltips(event, EPIC, (0xff804085), (0xffe3a084),
-                        (0xff804085), (48), (32), (4), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItemTags.IS_LEGENDARY)) {
-                int[] xOffset = new int[] {-5, -5, -4, -4};
-                int[] yOffset = new int[] {-17, -16, -17, -16, -20, -5};
-                int[] uOffset = new int[] {0, 0, 9, 9, 18, 18};
-                int[] vOffset = new int[] {0, 9, 0, 9, 0, 11};
-                int[] uWidth = new int[] {9, 9, 9, 9, 18, 10};
-                int[] vHeight = new int[] {9, 9, 9, 9, 11, 4};
-                TATooltipRenderUtils.renderTooltips(event, LEGENDARY, (0xff412051), (0xffe88a36),
-                        (0xff14182e), (48), (32), (4), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItemTags.IS_MYTHICAL)) {
-                int[] xOffset = new int[] {-5, -5, -4, -4};
-                int[] yOffset = new int[] {-17, -16, -17, -16, -20, -6};
-                int[] uOffset = new int[] {0, 0, 9, 9, 18, 18};
-                int[] vOffset = new int[] {0, 9, 0, 9, 0, 12};
-                int[] uWidth = new int[] {9, 9, 9, 9, 18, 14};
-                int[] vHeight = new int[] {9, 9, 9, 9, 12, 5};
-                TATooltipRenderUtils.renderTooltips(event, MYTHICAL, (0xff621748), (0xff3d003d),
-                        (0xff3d003d), (48), (32), (4), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItems.WHITE_CHOCOLATE.get())) {
-                int[] xOffset = new int[] {-6, -26, -5, -2};
-                int[] yOffset = new int[] {-18, -40, -18, -40, -22};
-                int[] uOffset = new int[] {0, 0, 11, 28, 22};
-                int[] vOffset = new int[] {0, 15, 0, 15, 0};
-                int[] uWidth = new int[] {11, 28, 11, 28, 18};
-                int[] vHeight = new int[] {11, 34, 11, 34, 15};
-                TATooltipRenderUtils.renderTooltips(event, WHITE_CHOCOLATE, (0xff694e36), (0xffddcea3),
-                        (0xff503a26), (128), (128), (5), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItems.RED_BOOK.get()) || itemStack.is(TAItems.RED_BOOK_RING.get())) {
-                int[] xOffset = new int[] {-5, -5, -4, -4};
-                int[] yOffset = new int[] {-17, -16, -17, -16, -25};
-                int[] uOffset = new int[] {0, 0, 9, 9, 18};
-                int[] vOffset = new int[] {0, 9, 0, 9, 0};
-                int[] uWidth = new int[] {9, 9, 9, 9, 23};
-                int[] vHeight = new int[] {9, 9, 9, 9, 16};
-                TATooltipRenderUtils.renderTooltips(event, RED_BOOK, (-267386864), (0xff6c337d),
-                        (-267386864), (48), (32), (4), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItems.STAR_OCEAN_CROSSBOW.get())) {
-                int[] xOffset = new int[] {-4, -4, -3, -4};
-                int[] yOffset = new int[] {-16, -18, -18, -16};
-                int[] uOffset = new int[] {0, 0, 8, 12};
-                int[] vOffset = new int[] {0, 9, 0, 11};
-                int[] uWidth = new int[] {8, 12, 9, 8};
-                int[] vHeight = new int[] {8, 10, 9, 8};
-                TATooltipRenderUtils.renderTooltips(event, STAR_OCEAN_CROSSBOW, (-267386864), (0xff217279),
-                        (-267386864), (64), (64), (0), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItems.DREAM_DYEING_CRYSTAL_FRAGMENT.get())) {
-                int[] xOffset = new int[] {-5, -5, -4, -4};
-                int[] yOffset = new int[] {-17, -16, -17, -16, -18, -7};
-                int[] uOffset = new int[] {0, 0, 9, 9, 18, 18};
-                int[] vOffset = new int[] {0, 9, 0, 9, 0, 9};
-                int[] uWidth = new int[] {9, 9, 9, 9, 14, 14};
-                int[] vHeight = new int[] {9, 9, 9, 9, 9, 5};
-                TATooltipRenderUtils.renderTooltips(event, DREAM_DYEING_CRYSTAL_FRAGMENT, (0xffcf5eac), (0xffffddeb),
-                        (0xffcf5eac), (48), (32), (2), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItems.CAT_BELL.get())) {
-                int[] xOffset = new int[] {-4, -4, -5, -5};
-                int[] yOffset = new int[] {-16, -17, -16, -17};
-                int[] uOffset = new int[] {0, 0, 9, 9};
-                int[] vOffset = new int[] {0, 9, 0, 9};
-                int[] uWidth = new int[] {9, 9, 9, 11};
-                int[] vHeight = new int[] {9, 9, 9, 9};
-                TATooltipRenderUtils.renderTooltips(event, CAT_BELL, (0xff272736), (0xff4b5bab),
-                        (0xff272736), (48), (32), (0), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            } else if (itemStack.is(TAItems.TSLAT_SWORD.get())) {
-                int[] xOffset = new int[] {-4, -4, -1, -1};
-                int[] yOffset = new int[] {-16, -13, -16, -13, -19};
-                int[] uOffset = new int[] {0, 0, 5, 5, 10};
-                int[] vOffset = new int[] {0, 5, 0, 5, 0};
-                int[] uWidth = new int[] {5, 5, 5, 5, 22};
-                int[] vHeight = new int[] {5, 5, 5, 5, 7};
-                TATooltipRenderUtils.renderTooltips(event, TSLAT_SWORD, (-267386864), (0xff37f037),
-                        (-267386864), (64), (64), (2), xOffset, yOffset, uOffset, vOffset, uWidth, vHeight);
-            }
-        }
-    }
-
     @SubscribeEvent
     public static void onRenderItemTooltips(ItemTooltipEvent event) {
-        boolean flag = event.getEntity().getAbilities().instabuild;
         ItemStack stack = event.getItemStack();
         List<Component> tooltip = event.getToolTip();
-        if (stack.getItem() instanceof ITooltipsItem tooltipsItem && tooltipsItem.isHasTooltips() && !flag) {
+        if (stack.getItem() instanceof ITooltipsItem tooltipsItem && tooltipsItem.isHasTooltips()) {
             showTooltips(tooltip, stack.getItem());
         }
 
--- src/main/java/cn/teampancake/theaurorian/common/utils/TATooltipRenderUtils.java
+++ src/main/java/cn/teampancake/theaurorian/common/utils/TATooltipRenderUtils.java
@@ -6,7 +6,6 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTextTooltip;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
-import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositioner;
 import net.minecraft.client.gui.screens.inventory.tooltip.TooltipRenderUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -28,7 +27,6 @@ public class TATooltipRenderUtils {
     public static void renderTooltips(
             RenderTooltipEvent.Pre event, ResourceLocation tooltips, int outerColor, int intermediateColor, int innerColor, int textureWidth,
             int textureHeight, int textTopOffset, int[] xOffset, int[] yOffset, int[] uOffset, int[] vOffset, int[] uWidth, int[] vHeight) {
-        ClientTooltipPositioner tooltipPositioner = event.getTooltipPositioner();
         List<ClientTooltipComponent> components = new ArrayList<>(event.getComponents());
         GuiGraphics graphics = event.getGraphics();
         Font font = event.getFont();
@@ -36,6 +34,7 @@ public class TATooltipRenderUtils {
         int mouseY = event.getY();
         int width = event.getScreenWidth();
         int height = event.getScreenHeight();
+        fixTooltipComponent(components, font, mouseX, width);
         int i0 = 0;
         int j0 = components.size() == 1 ? -2 : 0;
         for (ClientTooltipComponent component : components) {
@@ -45,16 +44,16 @@ public class TATooltipRenderUtils {
                 i0 = k;
             }
         }
+
         int tooltipWidth = i0, tooltipHeight = j0;
-        fixTooltipComponent(components, font, mouseX, width);
-        Vector2ic vector2ic = tooltipPositioner.positionTooltip(width, height, mouseX, mouseY, tooltipWidth, tooltipHeight);
-        int tooltipPosX = shouldFlip ? vector2ic.x() - 16 + i0 : vector2ic.x() + 12, tooltipPosY = vector2ic.y();
+        Vector2ic vector2ic = event.getTooltipPositioner().positionTooltip(width, height, mouseX, mouseY, tooltipWidth, tooltipHeight);
+        int tooltipPosX = shouldFlip ? mouseX - 16 - i0 : mouseX + 12, tooltipPosY = vector2ic.y();
         graphics.pose().pushPose();
         graphics.drawManaged(() -> {
             int i = tooltipPosX - 3;
-            int j = tooltipPosY - 3;
+            int j = tooltipPosY - 3 - 12;
             int k = tooltipWidth + 3 + 3;
-            int l = tooltipHeight + 3 + 3 + textTopOffset + 2;
+            int l = tooltipHeight + 3 + 5 + textTopOffset;
             TooltipRenderUtil.renderRectangle(graphics, i, j, k, l, 400, -267386864);
             TooltipRenderUtil.renderHorizontalLine(graphics, i, j - 1, k, 400, outerColor);
             TooltipRenderUtil.renderHorizontalLine(graphics, i, j + l, k, 400, outerColor);
@@ -67,27 +66,27 @@ public class TATooltipRenderUtils {
             TooltipRenderUtil.renderFrameGradient(graphics, i, j + 1, k, l, 400, intermediateColor, intermediateColor);
         });
         graphics.pose().translate(0.0F, 0.0F, 400.0F);
-        int k1 = tooltipPosY + textTopOffset;
+        int k1 = tooltipPosY + textTopOffset - 11;
         for (int l1 = 0; l1 < components.size(); ++l1) {
             ClientTooltipComponent component = components.get(l1);
-            int textWidth = component.getWidth(font);
-            int k0 = tooltipPosX + (tooltipWidth - textWidth) / 2;
+            int k0 = tooltipPosX + (tooltipWidth - component.getWidth(font)) / 2;
             component.renderText(font, k0, k1, graphics.pose().last().pose(), graphics.bufferSource());
             k1 += component.getHeight() + (l1 == 0 ? 2 : 0);
         }
-        k1 = tooltipPosY + textTopOffset;
+        k1 = tooltipPosY + textTopOffset - 11;
         RenderSystem.enableBlend();
-        int yd1 = mouseY + tooltipHeight + yOffset[1] + textTopOffset + 2;
-        int yd2 = mouseY + tooltipHeight + yOffset[3] + textTopOffset + 2;
-        graphics.blit(tooltips, tooltipPosX + xOffset[0], mouseY + yOffset[0], uOffset[0], vOffset[0], uWidth[0], vHeight[0], textureWidth, textureHeight);
+        int yd1 = tooltipPosY + tooltipHeight + yOffset[1] + textTopOffset + 2;
+        int yd2 = tooltipPosY + tooltipHeight + yOffset[3] + textTopOffset + 2;
+        graphics.blit(tooltips, tooltipPosX + xOffset[0], tooltipPosY + yOffset[0], uOffset[0], vOffset[0], uWidth[0], vHeight[0], textureWidth, textureHeight);
         graphics.blit(tooltips, tooltipPosX + xOffset[1], yd1, uOffset[1], vOffset[1], uWidth[1], vHeight[1], textureWidth, textureHeight);
-        graphics.blit(tooltips, tooltipPosX + tooltipWidth + xOffset[2], mouseY + yOffset[2], uOffset[2], vOffset[2], uWidth[2], vHeight[2], textureWidth, textureHeight);
+        graphics.blit(tooltips, tooltipPosX + tooltipWidth + xOffset[2], tooltipPosY + yOffset[2], uOffset[2], vOffset[2], uWidth[2], vHeight[2], textureWidth, textureHeight);
         graphics.blit(tooltips, tooltipPosX + tooltipWidth + xOffset[3], yd2, uOffset[3], vOffset[3], uWidth[3], vHeight[3], textureWidth, textureHeight);
         if (uWidth.length > 4) {
-            graphics.blit(tooltips, tooltipPosX + (tooltipWidth - uWidth[4]) / 2, mouseY + yOffset[4], uOffset[4], vOffset[4], uWidth[4], vHeight[4], textureWidth, textureHeight);
+            int x1 = tooltipPosX + (tooltipWidth - uWidth[4]) / 2;
+            graphics.blit(tooltips, x1, tooltipPosY + yOffset[4], uOffset[4], vOffset[4], uWidth[4], vHeight[4], textureWidth, textureHeight);
             if (uWidth.length > 5) {
                 int x2 = tooltipPosX + (tooltipWidth - uWidth[5]) / 2;
-                int y2 = mouseY + tooltipHeight + yOffset[5];
+                int y2 = tooltipPosY + tooltipHeight + yOffset[5];
                 graphics.blit(tooltips, x2, y2, uOffset[5], vOffset[5], uWidth[5], vHeight[5], textureWidth, textureHeight);
             }
         }
@@ -122,6 +121,7 @@ public class TATooltipRenderUtils {
         wrapLongLines(components, font, maxWidth);
     }
 
+    @SuppressWarnings("SuspiciousListRemoveInLoop")
     private static void wrapLongLines(List<ClientTooltipComponent> components, Font font, int maxSize) {
         for (int i = 0; i < components.size(); i++) {
             if (components.get(i) instanceof ClientTextTooltip clientTextTooltip) {

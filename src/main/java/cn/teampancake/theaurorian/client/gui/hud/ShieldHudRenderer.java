75597a2aec4e2c6e6b84613c05920dde2fc9c07a
glyceryl <494192175@qq.com>
2023-12-26 14:18:24 +0800

    Optimize the shield bar animation.

--- src/main/java/cn/teampancake/theaurorian/client/gui/hud/ShieldHudRenderer.java
+++ src/main/java/cn/teampancake/theaurorian/client/gui/hud/ShieldHudRenderer.java
@@ -16,67 +16,37 @@ import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import java.util.Map;
 
 public class ShieldHudRenderer {
+
     static Minecraft minecraft = Minecraft.getInstance();
     public static ResourceLocation SHIELD = AurorianMod.prefix("textures/gui/shield.png");
-    public static int[] size = {4,8,10,12,12,14,14,14,16,16,16,16,16,16,16,16,16,16,16};
-    public static int textureWidth = 24;
-    public static int textureHeight = 28;
-    public static IGuiOverlay SHIELD_OVERLAY = (forgeGui, guiGraphics, partialTicks, screenWidth, screenHeight) -> {
+    public static final int TEXTURE_WIDTH = 108;
+    public static final int TEXTURE_HEIGHT = 34;
+    public static IGuiOverlay SHIELD_OVERLAY = (forgeGui, guiGraphics, partialTicks, screenWidth, screenHeight) ->
+            render(forgeGui, guiGraphics, screenWidth, screenHeight, minecraft.player);
 
-        render(forgeGui, guiGraphics, screenWidth, screenHeight,minecraft.player);
-    };
+    @SuppressWarnings("unused")
     public static void render(ForgeGui forgeGui, GuiGraphics guiGraphics, int width, int height, Player player) {
         RenderSystem.enableBlend();
-        RenderSystem.setShaderTexture(0, SHIELD);
-        guiGraphics.blit(SHIELD, width - textureWidth, height - textureHeight, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
-
-
-        int xOffset = 1;
-        int yOffset = 5;
-        int yStart = height - yOffset;
-
-        int length = size.length;
-
-        ShieldCap.getCapability(player).ifPresent(shieldCap -> {
-
-        });
-
-
-
+        guiGraphics.blit(SHIELD, 3, height - TEXTURE_HEIGHT - 3, 0, 0, 42, 34, TEXTURE_WIDTH, TEXTURE_HEIGHT); //Render frame
+//        guiGraphics.blit(SHIELD, 13, height - TEXTURE_HEIGHT + 8, 42, 0, 22, 22, TEXTURE_WIDTH, TEXTURE_HEIGHT); //Render gray bar
+//        guiGraphics.blit(SHIELD, 10, height - TEXTURE_HEIGHT + 8, 86, 0, 22, 22, TEXTURE_WIDTH, TEXTURE_HEIGHT); //Render purple bar
+        ShieldCap.getCapability(player).ifPresent(shieldCap -> {});
         ShieldCap shieldCap = ShieldCap.getCapability(player).orElse(null);
-
-        if (shieldCap == null) {
-            return;
-        }
         Map<ResourceLocation, IShield> shieldMap = shieldCap.getShieldMap();
         float maxShieldValue = shieldCap.getMaxShieldValue();
-
-        int count = 0;
-        int sizeCount = 0;
         for (IShield value : shieldMap.values()) {
-            int color = value.getColor();
             float shield = value.getShield();
             float v = shield / maxShieldValue;
-            int v1 = (int) Math.floor(length * v);
-            count += v1;
-
-            if(count > length){
-                v1 = length - (count - v1);
-            }
-
-            for (int i = 0; i < v1; i++) {
-                int i1 = size[sizeCount];
-                int xStart = width - (textureWidth - i1) / 2 - i1 - xOffset;
-                guiGraphics.hLine(xStart, xStart + i1, yStart, color);
-                yStart--;
-                sizeCount++;
-            }
+            int v1 = (int) Math.floor(22 * v);
+            int y = height - TEXTURE_HEIGHT + 8 + 22 - v1;
+            guiGraphics.blit(SHIELD, 13, y, 64, 22 - v1, 22, v1, TEXTURE_WIDTH, TEXTURE_HEIGHT); //Render dynamic blue bar
         }
 
+        RenderSystem.disableBlend();
     }
 
-    public static void registerThirstOverlay(RegisterGuiOverlaysEvent event)
-    {
+    public static void registerThirstOverlay(RegisterGuiOverlaysEvent event) {
         event.registerAbove(VanillaGuiOverlay.FOOD_LEVEL.id(), "shield", SHIELD_OVERLAY);
     }
+
 }

e84ced926015b41f76d3d938208bca9db2025dd1
mlus-Asuka <1319237806@qq.com>
2023-12-28 20:37:32 +0800

    DimensionTeleport Fix.

--- src/main/java/cn/teampancake/theaurorian/common/mixin/PlayerListMixin.java
+++ src/main/java/cn/teampancake/theaurorian/common/mixin/PlayerListMixin.java
@@ -1,19 +1,22 @@
 package cn.teampancake.theaurorian.common.mixin;
 
+import cn.teampancake.theaurorian.AurorianMod;
 import cn.teampancake.theaurorian.common.config.AurorianConfig;
 import cn.teampancake.theaurorian.common.registry.TADimensions;
-import net.minecraft.network.Connection;
+import com.mojang.serialization.Dynamic;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
@@ -25,5 +28,16 @@ public abstract class PlayerListMixin {
             return value.getServer().getLevel(TADimensions.AURORIAN_DIMENSION);
         }
         return value;
+    }
+
+    @Redirect(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getLevel(Lnet/minecraft/resources/ResourceKey;)Lnet/minecraft/server/level/ServerLevel;"))
+    private ServerLevel modifyServerLevel(MinecraftServer instance, ResourceKey<Level> pDimension) {
+        if(AurorianConfig.CONFIG_DEFAULT_SPAWN_IN_AURORIAN_DIMENSION.get()){
+            ServerPlayer player=instance.getPlayerList().getPlayers().get(instance.getPlayerCount()-1);
+            CompoundTag compoundtag = instance.getPlayerList().load(player);
+            ResourceKey<Level> resourcekey = compoundtag != null ? DimensionType.parseLegacy(new Dynamic<>(NbtOps.INSTANCE, compoundtag.get("Dimension"))).resultOrPartial(AurorianMod.LOGGER::error).orElse(Level.OVERWORLD) : TADimensions.AURORIAN_DIMENSION;
+            return instance.getLevel(resourcekey);
+        }
+        return instance.getLevel(pDimension);
     }
 }

d53bd86a365cc6fba059085c865f8f74617e696c
XiaoHuNao <shiwenbin2250@gmail.com>
2024-01-02 02:02:28 +0800

    Fix the dimension of rebirth after death

--- src/main/java/cn/teampancake/theaurorian/common/mixin/PlayerListMixin.java
+++ src/main/java/cn/teampancake/theaurorian/common/mixin/PlayerListMixin.java
@@ -7,21 +7,35 @@ import com.mojang.serialization.Dynamic;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
 
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
-    @Redirect(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;load(Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/nbt/CompoundTag;"))
+    @Shadow @Final private MinecraftServer server;
+
+
+    @Redirect(method = "placeNewPlayer", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/server/players/PlayerList;load(Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/nbt/CompoundTag;"))
     private CompoundTag modifyLoad(PlayerList instance, ServerPlayer player) {
         if(AurorianConfig.CONFIG_DEFAULT_SPAWN_IN_AURORIAN_DIMENSION.get()){
             CompoundTag compoundtag = instance.load(player);
-            ResourceKey<Level> resourcekey = compoundtag != null ? DimensionType.parseLegacy(new Dynamic<>(NbtOps.INSTANCE, compoundtag.get("Dimension"))).resultOrPartial(AurorianMod.LOGGER::error).orElse(Level.OVERWORLD) : TADimensions.AURORIAN_DIMENSION;
+            ResourceKey<Level> resourcekey = compoundtag != null ?
+                    DimensionType.parseLegacy(new Dynamic<>(NbtOps.INSTANCE, compoundtag.get("Dimension")))
+                            .resultOrPartial(AurorianMod.LOGGER::error)
+                            .orElse(Level.OVERWORLD)
+                    : TADimensions.AURORIAN_DIMENSION;
             if(resourcekey == TADimensions.AURORIAN_DIMENSION) {
                 if(compoundtag == null)
                     compoundtag = new CompoundTag();
@@ -31,4 +45,23 @@ public abstract class PlayerListMixin {
         }
         return instance.load(player);
     }
+
+    @Redirect(method = "respawn", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/server/MinecraftServer;overworld()Lnet/minecraft/server/level/ServerLevel;"), require = 0)
+    private ServerLevel changeOverworld(MinecraftServer minecraftServer) {
+        if(AurorianConfig.CONFIG_DEFAULT_SPAWN_IN_AURORIAN_DIMENSION.get()){
+            return minecraftServer.getLevel(TADimensions.AURORIAN_DIMENSION);
+        }
+        return minecraftServer.overworld();
+    }
+
+
+    @ModifyVariable(method = "respawn", at = @At("STORE"), ordinal = 0)
+    private ServerLevel modifyServerLevel(ServerLevel serverlevel) {
+        if(AurorianConfig.CONFIG_DEFAULT_SPAWN_IN_AURORIAN_DIMENSION.get()){
+            return server.getLevel(TADimensions.AURORIAN_DIMENSION);
+        }
+        return serverlevel;
+    }
+
 }

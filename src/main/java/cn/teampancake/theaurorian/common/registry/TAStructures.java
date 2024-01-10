b3bcc3fe43817e5ca0be392e280d3e04eac4ab99
glyceryl <494192175@qq.com>
2024-01-11 00:55:45 +0800

    Add AT of some method in Structures class.

--- src/main/java/cn/teampancake/theaurorian/common/registry/TAStructures.java
+++ src/main/java/cn/teampancake/theaurorian/common/registry/TAStructures.java
@@ -1,16 +1,16 @@
 package cn.teampancake.theaurorian.common.registry;
 
 import cn.teampancake.theaurorian.AurorianMod;
-import cn.teampancake.theaurorian.common.data.datagen.tags.TABiomeTags;
 import cn.teampancake.theaurorian.common.level.structure.RuinsAltarStructure;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.Structures;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
-
-import java.util.Map;
 
 public class TAStructures {
 
@@ -21,9 +21,9 @@ public class TAStructures {
     }
 
     public static void bootstrap(BootstapContext<Structure> context) {
-        Structure.StructureSettings ruinsAltarSettings = new Structure.StructureSettings(
-                context.lookup(Registries.BIOME).getOrThrow(TABiomeTags.HAS_RUINS_ALTAR), Map.of(),
-                GenerationStep.Decoration.SURFACE_STRUCTURES, TerrainAdjustment.NONE);
+        HolderGetter<Biome> biomes = context.lookup(Registries.BIOME);
+        Structure.StructureSettings ruinsAltarSettings = Structures.structure(
+                biomes.getOrThrow(BiomeTags.IS_FOREST), TerrainAdjustment.NONE);
         context.register(RUINS_ALTAR, new RuinsAltarStructure(ruinsAltarSettings));
     }
 

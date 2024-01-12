c26cb36d4a7a264ffb9be39b8551266a32d66582
glyceryl <494192175@qq.com>
2024-01-13 02:37:36 +0800

    Code clean.

--- src/main/java/cn/teampancake/theaurorian/common/effect/FreezeEffect.java
+++ src/main/java/cn/teampancake/theaurorian/common/effect/FreezeEffect.java
@@ -1,7 +1,6 @@
 package cn.teampancake.theaurorian.common.effect;
 
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffectCategory;
@@ -9,7 +8,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.storage.loot.LootTable;
 
 public class FreezeEffect extends TAMobEffect {
 

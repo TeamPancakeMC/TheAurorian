065e8ce76ea99d015a1c91e5b4643374c2cb3814
glyceryl <494192175@qq.com>
2024-01-19 20:31:00 +0800

    Fix voxel shape problem of the urn.

--- src/main/java/cn/teampancake/theaurorian/common/blocks/UrnBlock.java
+++ src/main/java/cn/teampancake/theaurorian/common/blocks/UrnBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("deprecation")
@@ -16,7 +17,7 @@ public class UrnBlock extends Block {
 
     @Override
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return box(0.15, 0.0D, 0.15, 0.85, 0.95, 0.85);
+        return Shapes.box(0.15, 0.0D, 0.15, 0.85, 0.95, 0.85);
     }
 
 }

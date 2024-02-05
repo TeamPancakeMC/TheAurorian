e75389b8d473e30ad0efa04a6418f15b66e07c34
glyceryl <494192175@qq.com>
2024-02-05 23:53:42 +0800

    Fix a bug of the Developer Gift.

--- src/main/java/cn/teampancake/theaurorian/common/items/DeveloperGift.java
+++ src/main/java/cn/teampancake/theaurorian/common/items/DeveloperGift.java
@@ -1,15 +1,16 @@
 package cn.teampancake.theaurorian.common.items;
 
 import cn.teampancake.theaurorian.common.items.developer.IDeveloperItem;
+import cn.teampancake.theaurorian.common.utils.TACommonUtils;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class DeveloperGift extends Item {
@@ -20,14 +21,23 @@ public class DeveloperGift extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        List<Item> itemList = new ArrayList<>();
+        List<Item> itemList = this.getDeveloperItems();
         ItemStack stack = player.getItemInHand(usedHand);
-        ForgeRegistries.ITEMS.getValues().stream().filter(item -> item instanceof IDeveloperItem
-                && !itemList.contains(item)).forEach(itemList::add);
         stack.shrink(player.getAbilities().instabuild ? 0 : 1);
         int index = level.random.nextInt(itemList.size());
         player.addItem(itemList.get(index).getDefaultInstance());
         return InteractionResultHolder.consume(stack);
+    }
+
+    private List<Item> getDeveloperItems() {
+        HashSet<Item> items = new HashSet<>();
+        for (Item item : TACommonUtils.getKnownItems()) {
+            if (item instanceof IDeveloperItem) {
+                items.add(item);
+            }
+        }
+
+        return new ArrayList<>(items);
     }
 
 }

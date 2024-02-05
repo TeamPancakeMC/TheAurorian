d29f951e68dd37fba39f42bfdaaf36cc740f89ae
glyceryl <494192175@qq.com>
2024-02-05 23:55:06 +0800

    Add Paralysis effect to the White Chocolate.

--- src/main/java/cn/teampancake/theaurorian/common/items/developer/WhiteChocolate.java
+++ src/main/java/cn/teampancake/theaurorian/common/items/developer/WhiteChocolate.java
@@ -1,13 +1,16 @@
 package cn.teampancake.theaurorian.common.items.developer;
 
 import cn.teampancake.theaurorian.common.items.ITooltipsItem;
+import cn.teampancake.theaurorian.common.registry.TAMobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 
 public class WhiteChocolate extends Item implements IDeveloperItem, ITooltipsItem {
 
     public WhiteChocolate() {
-        super(new Properties().food(new FoodProperties.Builder().build()));
+        super(new Properties().food(new FoodProperties.Builder()
+                .effect(() -> new MobEffectInstance(TAMobEffects.PARALYSIS.get(), 1200), 1.0F).build()));
     }
 
 }

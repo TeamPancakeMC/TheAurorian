93e735ec20da5c7a21366d11ca71ff12d489393a
glyceryl <494192175@qq.com>
2024-02-11 18:31:46 +0800

    Mixin tweaks.

--- src/main/java/cn/teampancake/theaurorian/common/blocks/entity/AurorianFurnaceBlockEntity.java
+++ src/main/java/cn/teampancake/theaurorian/common/blocks/entity/AurorianFurnaceBlockEntity.java
@@ -2,22 +2,29 @@ package cn.teampancake.theaurorian.common.blocks.entity;
 
 import cn.teampancake.theaurorian.common.blocks.AurorianFurnace;
 import cn.teampancake.theaurorian.common.config.AurorianConfig;
-import cn.teampancake.theaurorian.common.mixin.InvokeAbstractFurnaceBlockEntity;
 import cn.teampancake.theaurorian.common.registry.TABlockEntityTypes;
 import cn.teampancake.theaurorian.common.registry.TABlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
+import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.FurnaceMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
+import javax.annotation.Nullable;
+
+@SuppressWarnings("unchecked")
 public class AurorianFurnaceBlockEntity extends AbstractFurnaceBlockEntity {
 
     public AurorianFurnaceBlockEntity(BlockPos pos, BlockState blockState) {
@@ -54,7 +61,7 @@ public class AurorianFurnaceBlockEntity extends AbstractFurnaceBlockEntity {
         if (blockEntity.isLit() || flag3 && flag2) {
             int i = blockEntity.getMaxStackSize();
             Recipe<?> recipe = flag2 ? blockEntity.quickCheck.getRecipeFor(blockEntity, level).orElse(null) : null;
-            if (!blockEntity.isLit() && ((InvokeAbstractFurnaceBlockEntity)blockEntity).canBurn(level.registryAccess(), recipe, blockEntity.items, i)) {
+            if (!blockEntity.isLit() && blockEntity.canBurn(level.registryAccess(), recipe, blockEntity.items, i)) {
                 int burnDuration = blockEntity.getBurnDuration(itemStack);
                 blockEntity.litTime = (int) (burnDuration - burnDuration * blockEntity.getChimneySpeedMultiplier());
                 blockEntity.litDuration = blockEntity.litTime;
@@ -71,12 +78,12 @@ public class AurorianFurnaceBlockEntity extends AbstractFurnaceBlockEntity {
                 }
             }
 
-            if (blockEntity.isLit() && ((InvokeAbstractFurnaceBlockEntity)blockEntity).canBurn(level.registryAccess(), recipe, blockEntity.items, i)) {
+            if (blockEntity.isLit() && blockEntity.canBurn(level.registryAccess(), recipe, blockEntity.items, i)) {
                 ++blockEntity.cookingProgress;
                 if (blockEntity.cookingProgress == blockEntity.cookingTotalTime) {
                     blockEntity.cookingProgress = 0;
                     blockEntity.cookingTotalTime = blockEntity.getSmeltTime();
-                    if (((InvokeAbstractFurnaceBlockEntity)blockEntity).burn(level.registryAccess(), recipe, blockEntity.items, i)) {
+                    if (blockEntity.burn(level.registryAccess(), recipe, blockEntity.items, i)) {
                         blockEntity.setRecipeUsed(recipe);
                     }
 
@@ -100,10 +107,54 @@ public class AurorianFurnaceBlockEntity extends AbstractFurnaceBlockEntity {
         }
     }
 
+    private boolean canBurn(RegistryAccess registryAccess, @Nullable Recipe<?> recipe, NonNullList<ItemStack> inventory, int maxStackSize) {
+        if (!inventory.get(0).isEmpty() && recipe != null) {
+            ItemStack itemStack = ((Recipe<WorldlyContainer>) recipe).assemble(this, registryAccess);
+            if (itemStack.isEmpty()) {
+                return false;
+            } else {
+                ItemStack itemStack1 = inventory.get(2);
+                if (itemStack1.isEmpty()) {
+                    return true;
+                } else if (!ItemStack.isSameItem(itemStack1, itemStack)) {
+                    return false;
+                } else if (itemStack1.getCount() + itemStack.getCount() <= maxStackSize && itemStack1.getCount() + itemStack.getCount() <= itemStack1.getMaxStackSize()) {
+                    return true;
+                } else {
+                    return itemStack1.getCount() + itemStack.getCount() <= itemStack.getMaxStackSize();
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private boolean burn(RegistryAccess registryAccess, @Nullable Recipe<?> recipe, NonNullList<ItemStack> inventory, int maxStackSize) {
+        if (recipe != null && this.canBurn(registryAccess, recipe, inventory, maxStackSize)) {
+            ItemStack itemStack = inventory.get(0);
+            ItemStack itemStack1 = ((Recipe<WorldlyContainer>) recipe).assemble(this, registryAccess);
+            ItemStack itemStack2 = inventory.get(2);
+            if (itemStack2.isEmpty()) {
+                inventory.set(2, itemStack1.copy());
+            } else if (itemStack2.is(itemStack1.getItem())) {
+                itemStack2.grow(itemStack1.getCount());
+            }
+
+            if (itemStack.is(Blocks.WET_SPONGE.asItem()) && !inventory.get(1).isEmpty() && inventory.get(1).is(Items.BUCKET)) {
+                inventory.set(1, new ItemStack(Items.WATER_BUCKET));
+            }
+
+            itemStack.shrink(1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public void setItem(int index, ItemStack stack) {
-        ItemStack itemstack = this.items.get(index);
-        boolean flag = !stack.isEmpty() && ItemStack.isSameItemSameTags(itemstack, stack);
+        ItemStack itemStack = this.items.get(index);
+        boolean flag = !stack.isEmpty() && ItemStack.isSameItemSameTags(itemStack, stack);
         this.items.set(index, stack);
         if (stack.getCount() > this.getMaxStackSize()) {
             stack.setCount(this.getMaxStackSize());

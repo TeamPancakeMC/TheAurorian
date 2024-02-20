a7a1e0937988c6cf1f567eee09879751b51ce17c
glyceryl <494192175@qq.com>
2024-02-20 10:02:03 +0800

    Fix a model problem of the Bread Beast.

--- src/main/java/cn/teampancake/theaurorian/client/model/entity/BreadBeastModel.java
+++ src/main/java/cn/teampancake/theaurorian/client/model/entity/BreadBeastModel.java
@@ -14,7 +14,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class BreadBeastModel<T extends BreadBeast> extends EntityModel<T> {
 
-    private final ModelPart head;
+    private final ModelPart neck;
+    private final ModelPart body;
     private final ModelPart rightHindLeg;
     private final ModelPart leftHindLeg;
     private final ModelPart rightFrontLeg;
@@ -24,34 +25,36 @@ public class BreadBeastModel<T extends BreadBeast> extends EntityModel<T> {
     private final ModelPart eatenTail2;
 
     public BreadBeastModel(ModelPart root) {
-        ModelPart body = root.getChild("body");
-        this.head = body.getChild("neck").getChild("head");
-        this.rightHindLeg = body.getChild("right_hind_leg");
-        this.leftHindLeg = body.getChild("left_hind_leg");
-        this.rightFrontLeg = body.getChild("right_front_leg");
-        this.leftFrontLeg = body.getChild("left_front_leg");
-        this.fullTail = body.getChild("tail_full");
-        this.eatenTail1 = body.getChild("tail_eaten_1");
-        this.eatenTail2 = body.getChild("tail_eaten_2");
+        ModelPart all = root.getChild("all");
+        this.body = all.getChild("body");
+        this.neck = all.getChild("neck");
+        this.rightHindLeg = all.getChild("right_hind_leg");
+        this.leftHindLeg = all.getChild("left_hind_leg");
+        this.rightFrontLeg = all.getChild("right_front_leg");
+        this.leftFrontLeg = all.getChild("left_front_leg");
+        this.fullTail = all.getChild("tail_full");
+        this.eatenTail1 = all.getChild("tail_eaten_1");
+        this.eatenTail2 = all.getChild("tail_eaten_2");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshDefinition = new MeshDefinition();
         PartDefinition partDefinition = meshDefinition.getRoot();
-        PartDefinition body = partDefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -5.0F, -10.0F, 12.0F, 10.0F, 20.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 5.75F, 0.0F));
-        PartDefinition neck = body.addOrReplaceChild("neck", CubeListBuilder.create(), PartPose.offset(0.0F, -0.5F, -9.25F));
+        PartDefinition all = partDefinition.addOrReplaceChild("all", CubeListBuilder.create(), PartPose.offset(0.0F, 5.75F, 0.0F));
+        all.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -5.0F, -10.0F, 12.0F, 10.0F, 20.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition neck = all.addOrReplaceChild("neck", CubeListBuilder.create(), PartPose.offset(0.0F, -0.5F, -9.25F));
         neck.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 30).addBox(-3.0F, -15.0F, -1.5F, 6.0F, 19.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.1309F, 0.0F, 0.0F));
         PartDefinition head = neck.addOrReplaceChild("head", CubeListBuilder.create().texOffs(24, 30).addBox(-3.5F, -5.0F, -5.0F, 7.0F, 6.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -12.0F, -2.0F));
         head.addOrReplaceChild("ear_right", CubeListBuilder.create().texOffs(0, -3).addBox(0.0F, 0.0F, -1.5F, 0.0F, 5.0F, 3.0F, new CubeDeformation(0.01F)), PartPose.offsetAndRotation(-3.25F, -2.5F, 3.25F, 0.2869F, -0.1464F, 0.2699F));
         head.addOrReplaceChild("ear_left", CubeListBuilder.create().texOffs(0, -3).mirror().addBox(0.0F, 0.0F, -1.5F, 0.0F, 5.0F, 3.0F, new CubeDeformation(0.01F)).mirror(false), PartPose.offsetAndRotation(3.25F, -2.5F, 3.25F, 0.2869F, 0.1464F, -0.2699F));
-        body.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(0, 55).addBox(-1.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.5F, 2.25F, -6.5F));
-        body.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 55).addBox(-1.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.25F, 2.25F, 6.5F));
-        body.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 55).mirror().addBox(-4.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(5.25F, 2.25F, 6.5F));
-        body.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(0, 55).mirror().addBox(-4.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(5.5F, 2.25F, -6.5F));
-        body.addOrReplaceChild("tail_full", CubeListBuilder.create().texOffs(20, 63).addBox(-3.0F, 0.0F, -0.25F, 6.0F, 5.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
-        body.addOrReplaceChild("tail_eaten_1", CubeListBuilder.create().texOffs(52, 65).addBox(-3.0F, 0.0F, -0.25F, 6.0F, 5.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
-        body.addOrReplaceChild("tail_eaten_2", CubeListBuilder.create().texOffs(55, 56).addBox(-3.0F, 0.0F, -0.25F, 6.0F, 5.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
-        body.addOrReplaceChild("tail_empty", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
+        all.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(0, 55).addBox(-1.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.5F, 2.25F, -6.5F));
+        all.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 55).addBox(-1.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.25F, 2.25F, 6.5F));
+        all.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 55).mirror().addBox(-4.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(5.25F, 2.25F, 6.5F));
+        all.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(0, 55).mirror().addBox(-4.0F, 0.0F, -2.0F, 5.0F, 16.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(5.5F, 2.25F, -6.5F));
+        all.addOrReplaceChild("tail_full", CubeListBuilder.create().texOffs(20, 63).addBox(-3.0F, 0.0F, -0.25F, 6.0F, 5.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
+        all.addOrReplaceChild("tail_eaten_1", CubeListBuilder.create().texOffs(52, 65).addBox(-3.0F, 0.0F, -0.25F, 6.0F, 5.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
+        all.addOrReplaceChild("tail_eaten_2", CubeListBuilder.create().texOffs(55, 56).addBox(-3.0F, 0.0F, -0.25F, 6.0F, 5.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
+        all.addOrReplaceChild("tail_empty", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, -3.0F, 10.0F, -0.3927F, 0.0F, 0.0F));
         return LayerDefinition.create(meshDefinition, 80, 80);
     }
 
@@ -65,7 +68,8 @@ public class BreadBeastModel<T extends BreadBeast> extends EntityModel<T> {
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        this.head.render(poseStack, vertexConsumer, packedLight, packedOverlay);
+        this.neck.render(poseStack, vertexConsumer, packedLight, packedOverlay);
+        this.body.render(poseStack, vertexConsumer, packedLight, packedOverlay);
         this.rightHindLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay);
         this.leftHindLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay);
         this.rightFrontLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay);

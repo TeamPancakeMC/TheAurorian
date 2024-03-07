e3b79d792b01dca0019a539dab0a15683d5ee89c
glyceryl <494192175@qq.com>
2024-03-07 23:15:46 +0800

    Continue to enhance the Concealment effect.

--- src/main/java/cn/teampancake/theaurorian/common/mixin/MixinLevelRenderer.java
+++ src/main/java/cn/teampancake/theaurorian/common/mixin/MixinLevelRenderer.java
@@ -6,6 +6,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +21,15 @@ public class MixinLevelRenderer {
     private void renderEntity(Entity entity, double camX, double camY, double camZ, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, CallbackInfo ci) {
         if (Minecraft.getInstance().getCameraEntity() instanceof LocalPlayer player && player.hasEffect(TAMobEffects.CONCEALMENT.get()) && entity != player) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "addParticle(Lnet/minecraft/core/particles/ParticleOptions;ZDDDDDD)V", at = @At(value = "HEAD"), cancellable = true)
+    public void addParticle(ParticleOptions options, boolean force, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, CallbackInfo ci) {
+        if (Minecraft.getInstance().getCameraEntity() instanceof LocalPlayer player && player.hasEffect(TAMobEffects.CONCEALMENT.get())) {
+            if (options.getType() == ParticleTypes.ENTITY_EFFECT) {
+                ci.cancel();
+            }
         }
     }
     
--- src/main/java/cn/teampancake/theaurorian/common/registry/TAMobEffects.java
+++ src/main/java/cn/teampancake/theaurorian/common/registry/TAMobEffects.java
@@ -14,9 +14,9 @@ public class TAMobEffects {
 
     public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, AurorianMod.MOD_ID);
     public static final RegistryObject<MobEffect> STUN = MOB_EFFECTS.register("stun", () -> new TAMobEffect(MobEffectCategory.HARMFUL, 0x8b0000)
-            .addAttributeModifier(Attributes.MOVEMENT_SPEED, "e8c3b0b0-3d6c-11eb-b378-0242ac130002", (-1), AttributeModifier.Operation.MULTIPLY_TOTAL));
+            .addAttributeModifier(Attributes.MOVEMENT_SPEED, "e8c3b0b0-3d6c-11eb-b378-0242ac130002", -1, AttributeModifier.Operation.MULTIPLY_TOTAL));
     public static final RegistryObject<MobEffect> PARALYSIS = MOB_EFFECTS.register("paralysis", () -> new TAMobEffect(MobEffectCategory.HARMFUL, 0xc09c72)
-            .addAttributeModifier(Attributes.MOVEMENT_SPEED, "3e4b0f9a-243e-4ea0-96ed-240437577f30", (-1), AttributeModifier.Operation.MULTIPLY_TOTAL));
+            .addAttributeModifier(Attributes.MOVEMENT_SPEED, "3e4b0f9a-243e-4ea0-96ed-240437577f30", -1, AttributeModifier.Operation.MULTIPLY_TOTAL));
     public static final RegistryObject<MobEffect> MOON_OF_VENGEANCE = MOB_EFFECTS.register("moon_of_vengeance", () -> new TAMobEffect(MobEffectCategory.BENEFICIAL, 0x960a17)
             .addAttributeModifier(Attributes.ATTACK_DAMAGE, "b8b5ad84-3a19-416a-870f-ac44bdee5954", 2.0F, AttributeModifier.Operation.MULTIPLY_TOTAL));
     public static final RegistryObject<MobEffect> OVERHEATING = MOB_EFFECTS.register("overheating", () -> new TAMobEffect(MobEffectCategory.HARMFUL, 0xdb5f39)
@@ -26,7 +26,7 @@ public class TAMobEffects {
     public static final RegistryObject<MobEffect> BROKEN = MOB_EFFECTS.register("broken", () -> new TAMobEffect(MobEffectCategory.HARMFUL, 0x808080));
     public static final RegistryObject<MobEffect> CRESCENT = MOB_EFFECTS.register("crescent", () -> new TAMobEffect(MobEffectCategory.BENEFICIAL, 0xd1b9a1));
     public static final RegistryObject<MobEffect> PRESSURE = MOB_EFFECTS.register("pressure", () -> new IncurableEffect(MobEffectCategory.HARMFUL, 0x714bdb));
-    public static final RegistryObject<MobEffect> CONCEALMENT = MOB_EFFECTS.register("concealment", () -> new TAMobEffect(MobEffectCategory.BENEFICIAL, 0x2b2b2b));
+    public static final RegistryObject<MobEffect> CONCEALMENT = MOB_EFFECTS.register("concealment", () -> new IncurableEffect(MobEffectCategory.HARMFUL, 0x2b2b2b));
     public static final RegistryObject<MobEffect> FALL_OF_MOON = MOB_EFFECTS.register("fall_of_moon", () -> new TAMobEffect(MobEffectCategory.BENEFICIAL, 0xb76ce6));
     public static final RegistryObject<MobEffect> BLESS_OF_MOON = MOB_EFFECTS.register("bless_of_moon", () -> new TAMobEffect(MobEffectCategory.BENEFICIAL, 0xe3d5aa));
     public static final RegistryObject<MobEffect> WARM = MOB_EFFECTS.register("warm", WarmEffect::new);

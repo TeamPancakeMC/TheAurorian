bd8cd0468df9e3a03117182d41ea10d6e2135778
glyceryl <494192175@qq.com>
2024-03-07 13:47:09 +0800

    Enhance the Concealment effect.

--- src/main/java/cn/teampancake/theaurorian/common/event/subscriber/ClientEventSubscriber.java
+++ src/main/java/cn/teampancake/theaurorian/common/event/subscriber/ClientEventSubscriber.java
@@ -10,17 +10,17 @@ import cn.teampancake.theaurorian.common.registry.TAMobEffects;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.*;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
+import net.minecraftforge.client.event.MovementInputUpdateEvent;
+import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import net.minecraftforge.client.event.ViewportEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -43,21 +43,10 @@ public class ClientEventSubscriber {
     }
 
     @SubscribeEvent
-    public static <T extends LivingEntity, M extends EntityModel<T>> void onRenderLiving(RenderLivingEvent.Pre<T, M> event) {
-        Entity cameraEntity = Minecraft.getInstance().getCameraEntity();
-        if (cameraEntity instanceof LocalPlayer player && player.hasEffect(TAMobEffects.CONCEALMENT.get())) {
-            event.setCanceled(true);
-        }
-    }
-
-    @SubscribeEvent
     public static void onViewpoint(ViewportEvent event) {
         Minecraft minecraft = Minecraft.getInstance();
         Entity cameraEntity = minecraft.getCameraEntity();
-        ClientLevel level = minecraft.level;
-        if (cameraEntity instanceof LocalPlayer player && level != null) {
-            boolean flag = player.hasEffect(TAMobEffects.CONCEALMENT.get());
-            level.entitiesForRendering().forEach(entity -> entity.setInvisible(flag));
+        if (cameraEntity instanceof LocalPlayer player) {
             if (player.hasEffect(TAMobEffects.CORRUPTION.get())) {
                 player.hurtTime = 0;
             }
---
+++ src/main/java/cn/teampancake/theaurorian/common/mixin/MixinLevelRenderer.java
@@ -0,0 +1,25 @@
+package cn.teampancake.theaurorian.common.mixin;
+
+import cn.teampancake.theaurorian.common.registry.TAMobEffects;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class MixinLevelRenderer {
+
+    @Inject(method = "renderEntity", at = @At(value = "HEAD"), cancellable = true)
+    private void renderEntity(Entity entity, double camX, double camY, double camZ, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, CallbackInfo ci) {
+        if (Minecraft.getInstance().getCameraEntity() instanceof LocalPlayer player && player.hasEffect(TAMobEffects.CONCEALMENT.get()) && entity != player) {
+            ci.cancel();
+        }
+    }
+    
+}

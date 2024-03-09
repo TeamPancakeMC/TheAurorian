9927479b908e229e86276f7407341bf1fca4b22d
Leo Lezury <68064905+LeoLezury@users.noreply.github.com>
2024-03-09 16:35:32 +0800

    more stuff about the crab

---
+++ src/main/java/cn/teampancake/theaurorian/common/entities/ai/GiantCrabDoNothingGoal.java
@@ -0,0 +1,34 @@
+package cn.teampancake.theaurorian.common.entities.ai;
+
+import cn.teampancake.theaurorian.common.entities.monster.SnowTundraGiantCrab;
+import cn.teampancake.theaurorian.common.entities.monster.SnowTundraGiantCrabHidePhase;
+import net.minecraft.world.entity.ai.goal.Goal;
+
+import java.util.EnumSet;
+
+public class GiantCrabDoNothingGoal extends Goal {
+    private final SnowTundraGiantCrab mob;
+
+    public GiantCrabDoNothingGoal(SnowTundraGiantCrab mob) {
+        setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK, Flag.JUMP));
+        this.mob = mob;
+    }
+
+    @Override
+    public boolean requiresUpdateEveryTick() {
+        return true;
+    }
+
+    public boolean canUse() {
+        return mob.getAttackState() == SnowTundraGiantCrabHidePhase.ID;
+    }
+
+    public boolean canContinueToUse() {
+        return mob.getAttackState() == SnowTundraGiantCrabHidePhase.ID;
+    }
+
+    public void tick() {
+        mob.getNavigation().stop();
+        mob.getLookControl().setLookAt(mob.getX() + 1, mob.getY(), mob.getZ(), 360, 360);
+    }
+}
---
+++ src/main/java/cn/teampancake/theaurorian/common/entities/monster/SnowTundraGiantCrabStartHidePhase.java
@@ -0,0 +1,34 @@
+package cn.teampancake.theaurorian.common.entities.monster;
+
+public class SnowTundraGiantCrabStartHidePhase extends AttackPhase<SnowTundraGiantCrab> {
+    public static final int ID = 2;
+
+    public SnowTundraGiantCrabStartHidePhase() {
+        super(ID, 1, 35, 500, SnowTundraGiantCrabHidePhase.ID);
+    }
+
+    @Override
+    public boolean canStart(SnowTundraGiantCrab entity, boolean coolDownOver) {
+        return coolDownOver && entity.getHealth() < entity.getMaxHealth() * 0.3;
+    }
+
+    @Override
+    public void onStart(SnowTundraGiantCrab entity) {
+        entity.triggerAnim("start_hide_controller", "start_hide_animation");
+    }
+
+    @Override
+    public void tick(SnowTundraGiantCrab entity) {
+
+    }
+
+    @Override
+    public boolean canContinue(SnowTundraGiantCrab entity) {
+        return true;
+    }
+
+    @Override
+    public void onStop(SnowTundraGiantCrab entity) {
+
+    }
+}

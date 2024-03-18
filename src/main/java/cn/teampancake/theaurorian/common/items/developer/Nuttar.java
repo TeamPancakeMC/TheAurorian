ca4b3ebc94c1e280b88b992af47bd401aebe0109
StarOcean <77849868+0Starocean0@users.noreply.github.com>
2024-03-18 09:56:14 +0800

    He should get the respect he deserved

---
+++ src/main/java/cn/teampancake/theaurorian/common/items/developer/Nuttar.java
@@ -0,0 +1,13 @@
+package cn.teampancake.theaurorian.common.items.developer;
+
+import cn.teampancake.theaurorian.common.items.ITooltipsItem;
+import net.minecraft.world.item.Item;
+
+public class RedBookRing extends Item implements IDeveloperItem, ITooltipsItem {
+
+    public Nuttar() {
+        super(new Properties());
+    }
+
+}
+//he should get the respect he deserved

bada902f15f43c7b98858283c95a8aee3e9f2e2a
glyceryl <494192175@qq.com>
2024-03-26 15:44:46 +0800

    Fix some bugs of the Aurorian Slime.

--- src/main/java/cn/teampancake/theaurorian/client/renderer/entity/AurorianSlimeRenderer.java
+++ src/main/java/cn/teampancake/theaurorian/client/renderer/entity/AurorianSlimeRenderer.java
@@ -3,10 +3,14 @@ package cn.teampancake.theaurorian.client.renderer.entity;
 import cn.teampancake.theaurorian.AurorianMod;
 import cn.teampancake.theaurorian.client.renderer.layers.TAModelLayers;
 import cn.teampancake.theaurorian.common.entities.monster.AurorianSlime;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.SlimeModel;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.SlimeOuterLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -15,6 +19,22 @@ public class AurorianSlimeRenderer extends MobRenderer<AurorianSlime, SlimeModel
 
     public AurorianSlimeRenderer(EntityRendererProvider.Context context) {
         super(context, new SlimeModel<>(context.bakeLayer(TAModelLayers.AURORIAN_SLIME)), 0.25F);
+        this.addLayer(new SlimeOuterLayer<>(this, context.getModelSet()));
+    }
+
+    @Override
+    public void render(AurorianSlime entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+        this.shadowRadius = 0.25F * (float)entity.getSize();
+        super.render(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
+    }
+
+    protected void scale(AurorianSlime livingEntity, PoseStack poseStack, float partialTickTime) {
+        poseStack.scale(0.999F, 0.999F, 0.999F);
+        poseStack.translate(0.0F, 0.001F, 0.0F);
+        float f1 = (float)livingEntity.getSize();
+        float f2 = Mth.lerp(partialTickTime, livingEntity.oSquish, livingEntity.squish) / (f1 * 0.5F + 1.0F);
+        float f3 = 1.0F / (f2 + 1.0F);
+        poseStack.scale(f3 * f1, 1.0F / f3 * f1, f3 * f1);
     }
 
     @Override
--- src/main/java/cn/teampancake/theaurorian/common/entities/monster/AurorianSlime.java
+++ src/main/java/cn/teampancake/theaurorian/common/entities/monster/AurorianSlime.java
@@ -1,7 +1,11 @@
 package cn.teampancake.theaurorian.common.entities.monster;
 
 import cn.teampancake.theaurorian.common.data.datagen.tags.TABlockTags;
+import cn.teampancake.theaurorian.common.registry.TAItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
@@ -17,6 +21,11 @@ public class AurorianSlime extends Slime {
 
     public static boolean checkSpawnRules(EntityType<AurorianSlime> aurorianSlime, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
         return level.getBlockState(pos.below()).is(TABlockTags.AUROTIAN_ANIMAL_UNSPAWNABLE_ON) && checkMobSpawnRules(aurorianSlime, level, spawnType, pos, random);
+    }
+
+    @Override
+    protected ParticleOptions getParticleType() {
+        return new ItemParticleOption(ParticleTypes.ITEM, TAItems.AURORIAN_SLIMEBALL.get().getDefaultInstance());
     }
 
 }

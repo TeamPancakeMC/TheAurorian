22f20ec3198c9bd3accd0615ad3161ee49e7f0e6
mlus <1319237806@qq.com>
2024-06-07 17:20:29 +0800

    Update Thirst Compat.

--- src/main/java/cn/teampancake/theaurorian/compat/ThirstWasTakenCompat.java
+++ src/main/java/cn/teampancake/theaurorian/compat/ThirstWasTakenCompat.java
@@ -1,14 +1,11 @@
 package cn.teampancake.theaurorian.compat;
 
 import cn.teampancake.theaurorian.common.registry.TAItems;
-import dev.ghen.thirst.api.ThirstHelper;
-import net.minecraftforge.client.event.RenderTooltipEvent;
+import dev.ghen.thirst.foundation.common.event.RegisterThirstValueEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ThirstWasTakenCompat {
-    public static boolean INITIALIZED = false;
 
     public static void init() {
         MinecraftForge.EVENT_BUS.register(ThirstWasTakenCompat.class);
@@ -17,34 +14,19 @@ public class ThirstWasTakenCompat {
     /**
      * Add Drinks here.
      */
-    public static void addDrinks() {
-        ThirstHelper.addDrink(TAItems.LAVENDER_TEA.get(), 10, 14);
-        ThirstHelper.addDrink(TAItems.SILK_BERRY_TEA.get(), 10, 14);
-        ThirstHelper.addDrink(TAItems.LAVENDER_SEEDY_TEA.get(), 10, 14);
-        ThirstHelper.addDrink(TAItems.PETUNIA_TEA.get(), 10, 14);
-        ThirstHelper.addDrink(TAItems.SLEEPING_BLACK_TEA.get(),10, 14);
-        ThirstHelper.addDrink(TAItems.BEPSI.get(), 6, 8);
-        ThirstHelper.addDrink(TAItems.AURORIAN_SPECIALTY_DRINK.get(), 8, 14);
-        ThirstHelper.addDrink(TAItems.MOONLIT_BLUEBERRY_SPECIALTY_DRINK.get(), 10, 14);
-        ThirstHelper.addFood(TAItems.SILK_SHROOM_STEW.get(), 4, 4);
-        ThirstHelper.addFood(TAItems.LAVENDER_SALAD.get(), 4, 4);
-        ThirstHelper.addFood(TAItems.SILENT_WOOD_FRUIT.get(), 1, 4);
-    }
-
     @SubscribeEvent
-    public static void registerDrinks(LivingEntityUseItemEvent.Finish event) {
-        if (!INITIALIZED) {
-            addDrinks();
-            INITIALIZED = true;
-        }
-    }
-
-    @SubscribeEvent
-    public static void registerDrinks(RenderTooltipEvent.GatherComponents event) {
-        if (!INITIALIZED) {
-            addDrinks();
-            INITIALIZED = true;
-        }
+    public static void registerDrinks(RegisterThirstValueEvent event){
+        event.addDrink(TAItems.LAVENDER_TEA.get(), 10, 14);
+        event.addDrink(TAItems.SILK_BERRY_TEA.get(), 10, 14);
+        event.addDrink(TAItems.LAVENDER_SEEDY_TEA.get(), 10, 14);
+        event.addDrink(TAItems.PETUNIA_TEA.get(), 10, 14);
+        event.addDrink(TAItems.SLEEPING_BLACK_TEA.get(),10, 14);
+        event.addDrink(TAItems.BEPSI.get(), 6, 8);
+        event.addDrink(TAItems.AURORIAN_SPECIALTY_DRINK.get(), 8, 14);
+        event.addDrink(TAItems.MOONLIT_BLUEBERRY_SPECIALTY_DRINK.get(), 10, 14);
+        event.addFood(TAItems.SILK_SHROOM_STEW.get(), 4, 4);
+        event.addFood(TAItems.LAVENDER_SALAD.get(), 4, 4);
+        event.addFood(TAItems.SILENT_WOOD_FRUIT.get(), 1, 4);
     }
 
 }

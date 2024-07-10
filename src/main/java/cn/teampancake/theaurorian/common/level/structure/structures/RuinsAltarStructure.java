0d03f6587e90f22aa9bb2dc22004b59e492444ed
glyceryl <494192175@qq.com>
2024-07-10 23:31:13 +0800

    Add a foundation to the Ruins Altar.

--- src/main/java/cn/teampancake/theaurorian/common/level/structure/structures/RuinsAltarStructure.java
+++ src/main/java/cn/teampancake/theaurorian/common/level/structure/structures/RuinsAltarStructure.java
@@ -2,16 +2,23 @@ package cn.teampancake.theaurorian.common.level.structure.structures;
 
 import cn.teampancake.theaurorian.AurorianMod;
 import cn.teampancake.theaurorian.common.level.structure.pieces.RuinsAltarPiece;
+import cn.teampancake.theaurorian.common.registry.TABlocks;
 import cn.teampancake.theaurorian.common.registry.TAStructureTypes;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.pieces.PiecesContainer;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
 import java.util.Optional;
@@ -40,6 +47,29 @@ public class RuinsAltarStructure extends Structure {
             ResourceLocation location = AurorianMod.prefix("ruins/ruins_altar/ruins_altar");
             builder.addPiece(new RuinsAltarPiece(manager, location, pos.above(), rotation));
         });
+    }
+
+    @Override
+    public void afterPlace(
+            WorldGenLevel level, StructureManager structureManager, ChunkGenerator chunkGenerator,
+            RandomSource random, BoundingBox boundingBox, ChunkPos chunkPos, PiecesContainer pieces) {
+        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+        BoundingBox box = pieces.calculateBoundingBox();
+        for (int k = boundingBox.minX(); k <= boundingBox.maxX(); ++k) {
+            for (int l = boundingBox.minZ(); l <= boundingBox.maxZ(); ++l) {
+                mutableBlockPos.set(k, box.minY(), l);
+                if (!level.isEmptyBlock(mutableBlockPos) && box.isInside(mutableBlockPos) && pieces.isInsidePiece(mutableBlockPos)) {
+                    for (int i1 = box.minY() - 1; i1 > level.getMinBuildHeight(); --i1) {
+                        mutableBlockPos.setY(i1);
+                        if (!level.isEmptyBlock(mutableBlockPos)) {
+                            break;
+                        }
+
+                        level.setBlock(mutableBlockPos, TABlocks.AURORIAN_STONE.get().defaultBlockState(), 2);
+                    }
+                }
+            }
+        }
     }
 
     @Override

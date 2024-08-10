dea102ea563655a16f72ab50251032243df4d587
mlus <1319237806@qq.com>
2024-08-10 15:07:24 +0800

    Baby Villager Renderer

--- src/main/java/cn/teampancake/theaurorian/client/renderer/entity/AurorianVillagerRenderer.java
+++ src/main/java/cn/teampancake/theaurorian/client/renderer/entity/AurorianVillagerRenderer.java
@@ -3,6 +3,7 @@ package cn.teampancake.theaurorian.client.renderer.entity;
 import cn.teampancake.theaurorian.AurorianMod;
 import cn.teampancake.theaurorian.common.entities.npc.AurorianVillager;
 import cn.teampancake.theaurorian.common.registry.TAVillagerProfession;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -14,6 +15,7 @@ public class AurorianVillagerRenderer extends LivingEntityRenderer<AurorianVilla
 
     private static final ResourceLocation ASTROLOGER  = AurorianMod.prefix("textures/entity/astrologer.png");
     private static final ResourceLocation ASTROLOGER_2  = AurorianMod.prefix("textures/entity/astrologer_2.png");
+
 
     public AurorianVillagerRenderer(EntityRendererProvider.Context context) {
         super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER_SLIM), true), 0.5f);
@@ -28,5 +30,18 @@ public class AurorianVillagerRenderer extends LivingEntityRenderer<AurorianVilla
 
         //Default Villager
         return ASTROLOGER;
+    }
+
+    @Override
+    protected void scale(AurorianVillager pLivingEntity, PoseStack pMatrixStack, float pPartialTickTime) {
+        float $$3 = 0.9375F;
+        if (pLivingEntity.isBaby()) {
+            $$3 *= 0.5F;
+            this.shadowRadius = 0.25F;
+        } else {
+            this.shadowRadius = 0.5F;
+        }
+
+        pMatrixStack.scale($$3, $$3, $$3);
     }
 }
--- src/main/java/cn/teampancake/theaurorian/common/entities/npc/AurorianVillager.java
+++ src/main/java/cn/teampancake/theaurorian/common/entities/npc/AurorianVillager.java
@@ -1,13 +1,16 @@
 package cn.teampancake.theaurorian.common.entities.npc;
 
+import cn.teampancake.theaurorian.common.registry.TAEntityTypes;
 import cn.teampancake.theaurorian.common.registry.TAVillagerType;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class AurorianVillager extends Villager {
 
@@ -17,5 +20,13 @@ public class AurorianVillager extends Villager {
 
     public static AttributeSupplier.@NotNull Builder createAttributes() {
         return Mob.createMobAttributes().add(Attributes.MOVEMENT_SPEED, 0.5).add(Attributes.FOLLOW_RANGE, 48.0);
+    }
+
+    @Nullable
+    @Override
+    public Villager getBreedOffspring(@NotNull ServerLevel pLevel, @NotNull AgeableMob pOtherParent) {
+        Villager villager = new AurorianVillager(TAEntityTypes.AURORIAN_VILLAGER.get(), pLevel);
+        villager.finalizeSpawn(pLevel, pLevel.getCurrentDifficultyAt(villager.blockPosition()), MobSpawnType.BREEDING, null, null);
+        return villager;
     }
 }

97a605345786dcd33e020f5153654193f5346a48
glyceryl <494192175@qq.com>
2024-08-22 00:14:05 +0800

    DG adjustment.

--- src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/TALootTableProvider.java
+++ src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/TALootTableProvider.java
@@ -1,6 +1,7 @@
 package cn.teampancake.theaurorian.common.data.datagen.provider;
 
 import cn.teampancake.theaurorian.common.data.datagen.loot.TABlockLoot;
+import cn.teampancake.theaurorian.common.data.datagen.loot.TAChestLoot;
 import cn.teampancake.theaurorian.common.data.datagen.loot.TAEntityLoot;
 import cn.teampancake.theaurorian.common.data.datagen.loot.TAFishingLoot;
 import net.minecraft.core.HolderLookup;
@@ -17,6 +18,7 @@ public class TALootTableProvider extends LootTableProvider {
     public TALootTableProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
         super(output, Set.of(), List.of(
                 new SubProviderEntry(TABlockLoot::new, LootContextParamSets.BLOCK),
+                new SubProviderEntry(TAChestLoot::new, LootContextParamSets.CHEST),
                 new SubProviderEntry(TAEntityLoot::new, LootContextParamSets.ENTITY),
                 new SubProviderEntry(TAFishingLoot::new, LootContextParamSets.FISHING)), provider);
     }
--- src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/TASoundProvider.java
+++ src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/TASoundProvider.java
@@ -3,9 +3,11 @@ package cn.teampancake.theaurorian.common.data.datagen.provider;
 import cn.teampancake.theaurorian.TheAurorian;
 import cn.teampancake.theaurorian.common.registry.TASoundEvents;
 import net.minecraft.data.PackOutput;
+import net.minecraft.sounds.SoundEvent;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.common.data.SoundDefinition;
 import net.neoforged.neoforge.common.data.SoundDefinitionsProvider;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class TASoundProvider extends SoundDefinitionsProvider {
 
@@ -15,16 +17,33 @@ public class TASoundProvider extends SoundDefinitionsProvider {
 
     @Override
     public void registerSounds() {
-        this.add(TASoundEvents.CRYSTALLINE_SWORD_USE, definition().with(sound(TASoundEvents.CRYSTALLINE_SWORD_USE.getId())));
-        this.add(TASoundEvents.CRYSTALLINE_SWORD_CHARGING, definition().with(sound(TASoundEvents.CRYSTALLINE_SWORD_CHARGING.getId())));
-        this.add(TASoundEvents.CRYSTALLINE_SWORD_SHOOT, definition().with(sound(TASoundEvents.CRYSTALLINE_SWORD_SHOOT.getId())));
-        this.add(TASoundEvents.WEEPING_WILLOW_BELL, definition().with(sound(TASoundEvents.WEEPING_WILLOW_BELL.getId())));
+        this.register(TASoundEvents.CRYSTALLINE_SWORD_USE);
+        this.register(TASoundEvents.CRYSTALLINE_SWORD_CHARGING);
+        this.register(TASoundEvents.CRYSTALLINE_SWORD_SHOOT);
+        this.register(TASoundEvents.WEEPING_WILLOW_BELL);
+        this.register(TASoundEvents.SNOW_TUNDRA_GIANT_CRAB_AMBIENT, 2);
+        this.register(TASoundEvents.SNOW_TUNDRA_GIANT_CRAB_DEATH, 2);
+        this.register(TASoundEvents.SNOW_TUNDRA_GIANT_CRAB_HURT, 3);
+        this.register(TASoundEvents.SNOW_TUNDRA_GIANT_CRAB_STEP, 2);
         this.add(TASoundEvents.BACKGROUND_MUSIC, definition().with(
                 sound(TheAurorian.prefix("music/aurorian_1"), SoundDefinition.SoundType.SOUND).stream(),
                 sound(TheAurorian.prefix("music/aurorian_2"), SoundDefinition.SoundType.SOUND).stream(),
                 sound(TheAurorian.prefix("music/aurorian_3"), SoundDefinition.SoundType.SOUND).stream(),
                 sound(TheAurorian.prefix("music/aurorian_4"), SoundDefinition.SoundType.SOUND).stream(),
                 sound(TheAurorian.prefix("music/aurorian_5"), SoundDefinition.SoundType.SOUND).stream()));
+    }
+
+    private void register(DeferredHolder<SoundEvent, SoundEvent> soundEvent){
+        this.add(soundEvent, definition().with(sound(soundEvent.getId())));
+    }
+
+    public void register(DeferredHolder<SoundEvent, SoundEvent> soundEvent,int variantType){
+        SoundDefinition definition = definition();
+        for (int i = 1; i <= variantType; i++) {
+            definition.with(sound(soundEvent.getId() + "_" + i));
+        }
+
+        this.add(soundEvent, definition);
     }
 
 }

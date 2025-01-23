59844faf4aec64e8415fe69b4052be5f266098b7
glyceryl <494192175@qq.com>
2025-01-23 22:23:50 +0800

    Adjust some features of the Aurorian Slime.

--- src/main/java/cn/teampancake/theaurorian/common/entities/monster/AurorianSlime.java
+++ src/main/java/cn/teampancake/theaurorian/common/entities/monster/AurorianSlime.java
@@ -1,17 +1,29 @@
 package cn.teampancake.theaurorian.common.entities.monster;
 
 import cn.teampancake.theaurorian.common.data.datagen.tags.TABlockTags;
+import cn.teampancake.theaurorian.common.entities.npc.AurorianVillager;
+import cn.teampancake.theaurorian.common.entities.npc.LadyKnight;
+import cn.teampancake.theaurorian.common.event.subscriber.LevelEventSubscriber;
 import cn.teampancake.theaurorian.common.registry.TAItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.jetbrains.annotations.Nullable;
 
 public class AurorianSlime extends Slime {
 
@@ -21,6 +33,42 @@ public class AurorianSlime extends Slime {
 
     public static boolean checkSpawnRules(EntityType<AurorianSlime> aurorianSlime, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
         return level.getBlockState(pos.below()).is(TABlockTags.AUROTIAN_ANIMAL_UNSPAWNABLE_ON) && checkMobSpawnRules(aurorianSlime, level, spawnType, pos, random);
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, AurorianVillager.class,
+                Boolean.TRUE, entity -> Math.abs(entity.getY() - this.getY()) <= 4.0F));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, LadyKnight.class,
+                Boolean.TRUE, entity -> Math.abs(entity.getY() - this.getY()) <= 4.0F));
+    }
+
+    @Override
+    public @Nullable SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType spawnType, @Nullable SpawnGroupData spawnGroupData) {
+        RandomSource random = level.getRandom();
+        int i = random.nextInt(3);
+        if (i < 2 && random.nextFloat() < 0.5F * difficulty.getSpecialMultiplier()) {
+            i++;
+        }
+
+        if (LevelEventSubscriber.phaseCode == 0 && random.nextFloat() <= 0.15F) {
+            i = 3;
+        }
+
+        int j = 1 << i;
+        this.setSize(j, true);
+        return spawnGroupData;
+    }
+
+    @Override
+    public boolean hurt(DamageSource source, float amount) {
+        boolean flag = super.hurt(source, amount);
+        if (flag && source.getEntity() instanceof Mob mob && this.random.nextFloat() <= 0.25F) {
+            mob.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30));
+        }
+
+        return flag;
     }
 
     @Override
--- src/main/java/cn/teampancake/theaurorian/common/mixin/MixinLivingEntity.java
+++ src/main/java/cn/teampancake/theaurorian/common/mixin/MixinLivingEntity.java
@@ -1,20 +1,43 @@
 package cn.teampancake.theaurorian.common.mixin;
 
+import cn.teampancake.theaurorian.common.entities.monster.AurorianSlime;
 import cn.teampancake.theaurorian.common.registry.TAEnchantments;
 import net.minecraft.core.Holder;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.neoforged.neoforge.common.extensions.ILivingEntityExtension;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+public abstract class MixinLivingEntity implements ILivingEntityExtension {
+
+    @Shadow public abstract ItemStack getItemBySlot(EquipmentSlot slot);
+
+    @Inject(method = "doHurtEquipment", at = @At(value = "HEAD"), cancellable = true)
+    public void doHurtEquipment(DamageSource damageSource, float damageAmount, EquipmentSlot[] slots, CallbackInfo ci) {
+        if (!(damageAmount <= 0.0F) && damageSource.getEntity() instanceof AurorianSlime) {
+            int i = (int) Math.max(1.0F, damageAmount / 4.0F) * 3;
+            for (EquipmentSlot slot : slots) {
+                ItemStack itemStack = this.getItemBySlot(slot);
+                if (itemStack.getItem() instanceof ArmorItem) {
+                    itemStack.hurtAndBreak(i, this.self(), slot);
+                }
+            }
+
+            ci.cancel();
+        }
+    }
 
     @Inject(method = "getDamageAfterArmorAbsorb", at = @At(value = "HEAD"), cancellable = true)
     protected void getDamageAfterArmorAbsorb(DamageSource damageSource, float damageAmount, CallbackInfoReturnable<Float> cir) {

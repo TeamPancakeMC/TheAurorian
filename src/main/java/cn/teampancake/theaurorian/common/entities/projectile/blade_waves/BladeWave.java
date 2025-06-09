161752044c2f31220886f79a15b0da61a697d2a7
glyceryl <494192175@qq.com>
2025-06-09 15:39:41 +0800

    Fix the Blade Wave cannot range attack.

--- src/main/java/cn/teampancake/theaurorian/common/entities/projectile/blade_waves/BladeWave.java
+++ src/main/java/cn/teampancake/theaurorian/common/entities/projectile/blade_waves/BladeWave.java
@@ -1,16 +1,12 @@
 package cn.teampancake.theaurorian.common.entities.projectile.blade_waves;
 
 import cn.teampancake.theaurorian.common.registry.TAEntityTypes;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
@@ -32,28 +28,20 @@ public class BladeWave extends AbstractHurtingProjectile {
     }
 
     @Override
-    protected void onHitBlock(BlockHitResult result) {
-        super.onHitBlock(result);
-        Level level = this.level();
-        if (!level.isClientSide()) {
-            EntityDimensions dimensions = this.getType().getDimensions();
-            int x = Mth.ceil(dimensions.width() / 2.0F) * 0;
-            int y = Mth.ceil(dimensions.height() / 2.0F) * 0;
-            BlockPos.withinManhattan(this.blockPosition(), x, y, x).forEach(
-                    tempPos -> level.destroyBlock(tempPos, Boolean.FALSE));
-            this.discard();
-        }
-    }
-
-    @Override
-    protected void onHitEntity(EntityHitResult result) {
-        if (this.getOwner() instanceof Mob mob) {
-            Entity entity = result.getEntity();
-            double value = mob.getAttributeValue(Attributes.ATTACK_DAMAGE);
-            entity.hurt(this.damageSources().mobAttack(mob), (float) value);
-            if (entity instanceof LivingEntity livingEntity) {
-                livingEntity.invulnerableTime = 0;
-            }
+    public void tick() {
+        super.tick();
+        Vec3 vec3 = this.getDeltaMovement();
+        this.setDeltaMovement(new Vec3(vec3.x, 0.0D, vec3.z));
+        if (!this.level().isClientSide() && this.getOwner() instanceof Mob mob) {
+            this.level().getEntitiesOfClass(Entity.class, this.getBoundingBox(), entity -> 
+                entity != this && entity != this.getOwner() && entity.isAlive()
+            ).forEach(entity -> {
+                double value = mob.getAttributeValue(Attributes.ATTACK_DAMAGE);
+                entity.hurt(this.damageSources().mobAttack(mob), (float) value);
+                if (entity instanceof LivingEntity livingEntity) {
+                    livingEntity.invulnerableTime = 0;
+                }
+            });
         }
     }
 

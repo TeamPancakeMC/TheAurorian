e030f82fb7c66a37019c1e9c544f7d338fd15492
glyceryl <494192175@qq.com>
2025-06-15 03:01:56 +0800

    优化“大地之源”组件的代码。

--- src/main/java/cn/teampancake/theaurorian/common/components/SourceOfTerra.java
+++ src/main/java/cn/teampancake/theaurorian/common/components/SourceOfTerra.java
@@ -4,6 +4,7 @@ import cn.teampancake.theaurorian.TheAurorian;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.ByteBufCodecs;
@@ -14,23 +15,21 @@ import net.minecraft.world.item.component.TooltipProvider;
 
 import java.util.function.Consumer;
 
-public record SourceOfTerra(String dimension, int selectedX, int selectedY, int selectedZ) implements TooltipProvider {
+public record SourceOfTerra(String dimension, BlockPos selectedPos) implements TooltipProvider {
 
     public static final Codec<SourceOfTerra> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.fieldOf("dimension").forGetter(SourceOfTerra::dimension),
-            Codec.INT.fieldOf("selected_x").forGetter(SourceOfTerra::selectedX),
-            Codec.INT.fieldOf("selected_y").forGetter(SourceOfTerra::selectedY),
-            Codec.INT.fieldOf("selected_z").forGetter(SourceOfTerra::selectedZ)).apply(instance, SourceOfTerra::new));
+            BlockPos.CODEC.fieldOf("selected_pos").forGetter(SourceOfTerra::selectedPos)).apply(instance, SourceOfTerra::new));
     public static final StreamCodec<FriendlyByteBuf, SourceOfTerra> STREAM_CODEC = StreamCodec.composite(
-            ByteBufCodecs.STRING_UTF8, SourceOfTerra::dimension, ByteBufCodecs.INT, SourceOfTerra::selectedX,
-            ByteBufCodecs.INT, SourceOfTerra::selectedY, ByteBufCodecs.INT, SourceOfTerra::selectedZ, SourceOfTerra::new);
+            ByteBufCodecs.STRING_UTF8, SourceOfTerra::dimension, BlockPos.STREAM_CODEC, SourceOfTerra::selectedPos, SourceOfTerra::new);
 
     @Override
     public void addToTooltip(Item.TooltipContext context, Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag) {
         if (!this.dimension.isEmpty()) {
             String key = "tooltips." + TheAurorian.MOD_ID + ".source_of_terra.dimension";
-            String s = " (" + this.selectedX + ", " + this.selectedY + ", " + this.selectedZ + ")";
-            tooltipAdder.accept(Component.translatable(key).append(this.dimension).withStyle(ChatFormatting.LIGHT_PURPLE).append(s));
+            String s = " (" + this.selectedPos.toShortString() + ")";
+            tooltipAdder.accept(Component.translatable(key).append(this.dimension)
+                    .withStyle(ChatFormatting.LIGHT_PURPLE).append(s));
         }
     }
 
--- src/main/java/cn/teampancake/theaurorian/common/event/subscriber/BlockEventSubscriber.java
+++ src/main/java/cn/teampancake/theaurorian/common/event/subscriber/BlockEventSubscriber.java
@@ -2,6 +2,7 @@ package cn.teampancake.theaurorian.common.event.subscriber;
 
 import cn.teampancake.theaurorian.TheAurorian;
 import cn.teampancake.theaurorian.common.components.SourceOfTerra;
+import cn.teampancake.theaurorian.common.registry.TAAttachmentTypes;
 import cn.teampancake.theaurorian.common.registry.TADataComponents;
 import cn.teampancake.theaurorian.common.registry.TAEnchantments;
 import net.minecraft.core.BlockPos;
@@ -23,9 +24,11 @@ import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.event.level.BlockDropsEvent;
 
 import java.util.List;
+import java.util.UUID;
 
 @EventBusSubscriber(modid = TheAurorian.MOD_ID)
 public class BlockEventSubscriber {
@@ -33,29 +36,46 @@ public class BlockEventSubscriber {
     @SubscribeEvent
     public static void onBlockDrops(BlockDropsEvent event) {
         List<ItemEntity> drops = event.getDrops();
-        if (!drops.isEmpty() && event.getBreaker() instanceof Player player) {
+        if (event.getBreaker() instanceof Player player) {
             ItemStack itemInHand = player.getItemInHand(player.getUsedItemHand());
             DataComponentType<SourceOfTerra> componentType = TADataComponents.SOURCE_OF_TERRA.get();
+            AttachmentType<List<UUID>> attachmentType = TAAttachmentTypes.BINDING_PLAYER_UUIDS.get();
+            BlockEntity currentBlockEntity = event.getBlockEntity();
+            if (currentBlockEntity != null) {
+                List<UUID> uuidList = currentBlockEntity.getData(attachmentType);
+                if (!uuidList.isEmpty()) {
+                    uuidList.forEach(uuid -> {
+                        if (event.getLevel().getPlayerByUUID(uuid) instanceof ServerPlayer serverPlayer) {
+                            serverPlayer.sendSystemMessage(Component.translatable("message.source_of_terra.destroy"));
+                        }
+                    });
+
+                    return;
+                }
+            }
+
             if (itemInHand.getEnchantmentLevel(TAEnchantments.get(player.level(), TAEnchantments.SOURCE_OF_TERRA)) > 0) {
                 SourceOfTerra sourceOfTerra = itemInHand.get(componentType);
-                if (sourceOfTerra != null) {
+                if (!drops.isEmpty() && sourceOfTerra != null) {
                     ResourceLocation levelResource = ResourceLocation.parse(sourceOfTerra.dimension());
                     ResourceKey<Level> levelKey = ResourceKey.create(Registries.DIMENSION, levelResource);
                     MinecraftServer server = player.level().getServer();
                     if (server != null) {
                         ServerLevel targetLevel = server.getLevel(levelKey);
                         if (targetLevel != null) {
-                            int selectedX = sourceOfTerra.selectedX();
-                            int selectedY = sourceOfTerra.selectedY();
-                            int selectedZ = sourceOfTerra.selectedZ();
+                            BlockPos selectedPos = sourceOfTerra.selectedPos();
+                            int selectedX = selectedPos.getX();
+                            int selectedY = selectedPos.getY();
+                            int selectedZ = selectedPos.getZ();
                             BlockPos targetPos = new BlockPos(selectedX, selectedY, selectedZ);
                             BlockState state = targetLevel.getBlockState(targetPos);
                             BlockEntity blockEntity = targetLevel.getBlockEntity(targetPos);
                             if (state.hasBlockEntity() && blockEntity instanceof Container container) {
                                 drops.forEach(itemEntity -> HopperBlockEntity.addItem(container, itemEntity));
-                            } else if (player instanceof ServerPlayer serverPlayer) {
-                                serverPlayer.sendSystemMessage(Component.translatable("message.source_of_terra.invalid"));
-                                itemInHand.remove(componentType);
+                            } else {
+                                if (player instanceof ServerPlayer serverPlayer) {
+                                    serverPlayer.sendSystemMessage(Component.translatable("message.source_of_terra.invalid"));
+                                }
                             }
                         }
                     }

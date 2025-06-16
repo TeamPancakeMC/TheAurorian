3437100eaedc575269d47861fb7ba8c8921e666f
glyceryl <494192175@qq.com>
2025-06-16 12:50:43 +0800

    优化极光钢剑及其监听事件的部分代码。

--- src/main/java/cn/teampancake/theaurorian/common/items/tool/AurorianSteelSword.java
+++ src/main/java/cn/teampancake/theaurorian/common/items/tool/AurorianSteelSword.java
@@ -12,8 +12,10 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.util.Unit;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -33,48 +35,29 @@ public class AurorianSteelSword extends SwordItem {
     }
 
     @Override
+    public float getAttackDamageBonus(Entity target, float damage, DamageSource damageSource) {
+        if (damageSource.getEntity() instanceof Player player && player.hasEffect(TAMobEffects.HOLINESS)) {
+            return target.getType().is(EntityTypeTags.UNDEAD) ? 2.0F : 1.0F;
+        }
+
+        return 0.0F;
+    }
+
+    @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         if (attacker instanceof Player player) {
-            if (this.isEasterEggPlayer(player)) {
+            if (!player.level().isClientSide() && this.isEasterEggPlayer(player)) {
                 target.hurt(target.damageSources().playerAttack(player), 1202.0F);
-                if (!player.level().isClientSide()) {
-                    String key = "messages." + this.getDescriptionId() + ".uuz.1";
-                    player.displayClientMessage(Component.translatable(key, player.getName()), true);
-                }
-            } else if (player.hasEffect(TAMobEffects.HOLINESS)) {
-                this.applyExtraDamage(target, player);
-                if (target.isDeadOrDying()) {
-                    this.extendHolinessEffect(player, this.isUndead(target));
-                }
+                String key = "messages." + this.getDescriptionId() + ".uuz.1";
+                player.displayClientMessage(Component.translatable(key, player.getName()), true);
             }
 
             if (!player.getAbilities().instabuild) {
-                stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(attacker.getUsedItemHand()));
+                stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(player.getUsedItemHand()));
             }
         }
 
         return true;
-    }
-
-    private boolean isEasterEggPlayer(Player player) {
-        return player.getStringUUID().equals("6b0f5181-a732-4ee2-b53a-c5a05d6af32e");
-    }
-
-    private void applyExtraDamage(LivingEntity target, Player player) {
-        float extraDamage = this.isUndead(target) ? 2.0F : 1.0F;
-        target.hurt(target.damageSources().playerAttack(player), extraDamage);
-    }
-
-    private void extendHolinessEffect(Player player, boolean isUndead) {
-        MobEffectInstance currentEffect = player.getEffect(TAMobEffects.HOLINESS);
-        if (currentEffect != null) {
-            int extraDuration = isUndead ? 60 : 30;
-            currentEffect.duration += extraDuration;
-        }
-    }
-
-    private boolean isUndead(LivingEntity entity) {
-        return entity.getType().is(EntityTypeTags.UNDEAD);
     }
 
     @Override
@@ -85,10 +68,8 @@ public class AurorianSteelSword extends SwordItem {
             player.displayClientMessage(Component.translatable(key, player.getName()), false);
             player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 1200, 2));
             player.addEffect(new MobEffectInstance(TAMobEffects.HOLINESS, 400));
-            return InteractionResultHolder.sidedSuccess(itemInHand, level.isClientSide());
         }
 
-        player.getCooldowns().addCooldown(this, 400);
         if (!level.isClientSide) {
             player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 600, 1));
             player.addEffect(new MobEffectInstance(TAMobEffects.HOLINESS, 200));
@@ -98,8 +79,13 @@ public class AurorianSteelSword extends SwordItem {
             }
         }
 
+        player.getCooldowns().addCooldown(this, 400);
         player.awardStat(Stats.ITEM_USED.get(this));
         return InteractionResultHolder.sidedSuccess(itemInHand, level.isClientSide());
+    }
+
+    private boolean isEasterEggPlayer(Player player) {
+        return player.getStringUUID().equals("6b0f5181-a732-4ee2-b53a-c5a05d6af32e");
     }
 
 }

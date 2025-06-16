d0f19b030344bef9fbe1f32427e4633438aabe2f
glyceryl <494192175@qq.com>
2025-06-16 12:49:24 +0800

    对之前新增的进度系统进行优化。

--- src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/TAAdvancementProvider.java
+++ src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/TAAdvancementProvider.java
@@ -2,27 +2,18 @@ package cn.teampancake.theaurorian.common.data.datagen.provider;
 
 import cn.teampancake.theaurorian.TheAurorian;
 import cn.teampancake.theaurorian.common.data.datagen.tags.TAEntityTags;
-import cn.teampancake.theaurorian.common.registry.TAEntityTypes;
+import cn.teampancake.theaurorian.common.registry.TADimensions;
 import cn.teampancake.theaurorian.common.registry.TAItems;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
-import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.critereon.ChangeDimensionTrigger;
 import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.KilledTrigger;
-import net.minecraft.advancements.critereon.TagPredicate;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.EntityTypeTags;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
@@ -32,50 +23,33 @@ import java.util.function.Consumer;
 
 public class TAAdvancementProvider extends AdvancementProvider {
 
+    private static final ResourceLocation BACKGROUND_LOCATION = TheAurorian.prefix("textures/block/aurorian_stone_bricks.png");
+
     public TAAdvancementProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, ExistingFileHelper existingFileHelper) {
         super(output, registries, existingFileHelper, List.of(new TAAdvancements()));
     }
 
     private static class TAAdvancements implements AdvancementGenerator {
+
         @Override
         public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> consumer, ExistingFileHelper existingFileHelper) {
-            // 第一个成就：进入极光维度
-            AdvancementHolder firstTraveler = Advancement.Builder.advancement()
-                    .display(
+            AdvancementHolder enterAurorian = Advancement.Builder.advancement().display(
                             TAItems.AURORIAN_CRYSTAL.get(),
-                            Component.translatable("advancement.theaurorian.first_traveler.title"),
-                            Component.translatable("advancement.theaurorian.first_traveler.desc"),
-                            ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/advancements/backgrounds/adventure.png"),
-                            AdvancementType.TASK,
-                            true,
-                            true,
-                            false
-                    )
-                    .addCriterion("enter_aurorian", ChangeDimensionTrigger.TriggerInstance.changedDimensionTo(
-                            ResourceKey.create(ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath("minecraft", "dimension")), 
-                                    TheAurorian.prefix("the_aurorian"))
-                    ))
-                    .requirements(AdvancementRequirements.Strategy.OR)
-                    .save(consumer, TheAurorian.MOD_ID + ":first_traveler");
-            
-            // 第二个成就：击杀极光生物
-            Advancement.Builder.advancement()
-                    .parent(firstTraveler)
-                    .display(
+                            Component.translatable("advancement.theaurorian.enter_aurorian.title"),
+                            Component.translatable("advancement.theaurorian.enter_aurorian.desc"),
+                            BACKGROUND_LOCATION, AdvancementType.TASK, true, true, false)
+                    .addCriterion("entered_aurorian", ChangeDimensionTrigger.TriggerInstance.changedDimensionTo(TADimensions.AURORIAN_DIMENSION))
+                    .save(consumer, TheAurorian.MOD_ID + ":enter_aurorian");
+            Advancement.Builder.advancement().parent(enterAurorian).display(
                             TAItems.SILENT_WOOD_SWORD.get(),
-                            Component.translatable("advancement.theaurorian.not_friendly.title"),
-                            Component.translatable("advancement.theaurorian.not_friendly.desc"),
-                            ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/advancements/backgrounds/adventure.png"),
-                            AdvancementType.TASK,
-                            true,
-                            true,
-                            false
-                    )
-                    .addCriterion("kill_aurorian_mob", KilledTrigger.TriggerInstance.playerKilledEntity(
-                            EntityPredicate.Builder.entity().of(TAEntityTags.AURORIAN_MOBS)
-                    ))
-                    .requirements(AdvancementRequirements.Strategy.OR)
-                    .save(consumer, TheAurorian.MOD_ID + ":not_friendly");
+                            Component.translatable("advancement.theaurorian.kill_aurorian_mob.title"),
+                            Component.translatable("advancement.theaurorian.kill_aurorian_mob.desc"),
+                            null, AdvancementType.TASK, true, true, false)
+                    .addCriterion("killed_aurorian_mob", KilledTrigger.TriggerInstance.playerKilledEntity(
+                            EntityPredicate.Builder.entity().of(TAEntityTags.AURORIAN_MOBS)))
+                    .save(consumer, TheAurorian.MOD_ID + ":kill_aurorian_mob");
         }
+
     }
+
 } 
--- src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/tag/TAEntityTagsProvider.java
+++ src/main/java/cn/teampancake/theaurorian/common/data/datagen/provider/tag/TAEntityTagsProvider.java
@@ -4,11 +4,9 @@ import cn.teampancake.theaurorian.TheAurorian;
 import cn.teampancake.theaurorian.common.data.datagen.tags.TAEntityTags;
 import cn.teampancake.theaurorian.common.registry.TAEntityTypes;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
 import net.minecraft.tags.EntityTypeTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -24,6 +22,7 @@ public class TAEntityTagsProvider extends EntityTypeTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
+        TAEntityTypes.ENTITY_TYPES.getEntries().forEach(holder -> this.tag(TAEntityTags.AURORIAN_MOBS).add(holder.get()));
         this.tag(EntityTypeTags.FALL_DAMAGE_IMMUNE).add(TAEntityTypes.AURORIAN_PIXIE.get(), TAEntityTypes.MOON_QUEEN.get(),
                 TAEntityTypes.CRYSTALLINE_SPRITE.get()).addTag(TAEntityTags.SPIDERLING);
         this.tag(EntityTypeTags.UNDEAD).add(TAEntityTypes.RUNESTONE_KEEPER.value(),
@@ -39,44 +38,6 @@ public class TAEntityTagsProvider extends EntityTypeTagsProvider {
         this.tag(TAEntityTags.AURORIAN_BOSS).add(TAEntityTypes.MOONLIGHT_KNIGHT.get(), TAEntityTypes.RUNESTONE_KEEPER.get(),
                 TAEntityTypes.SPIDER_MOTHER.get(), TAEntityTypes.MOON_QUEEN.get());
         this.tag(Tags.EntityTypes.BOSSES).addTag(TAEntityTags.AURORIAN_BOSS);
-        
-        // 添加极光生物标签
-        this.tag(TAEntityTags.AURORIAN_MOBS)
-            .add(TAEntityTypes.AURORIAN_PIG.get())
-            .add(TAEntityTypes.AURORIAN_SHEEP.get())
-            .add(TAEntityTypes.AURORIAN_COW.get())
-            .add(TAEntityTypes.AURORIAN_RABBIT.get())
-            .add(TAEntityTypes.AURORIAN_PIXIE.get())
-            .add(TAEntityTypes.AURORIAN_SLIME.get())
-            .add(TAEntityTypes.AURORIAN_VILLAGER.get())
-            .add(TAEntityTypes.AURORIAN_WINGED_FISH.get())
-            .add(TAEntityTypes.BLUE_TAIL_WOLF.get())
-            .add(TAEntityTypes.BREAD_BEAST.get())
-            .add(TAEntityTypes.CAVE_DWELLER.get())
-            .add(TAEntityTypes.CRYSTALLINE_SPRITE.get())
-            .add(TAEntityTypes.DISTURBED_HOLLOW.get())
-            .add(TAEntityTypes.FLOWER_LEECH.get())
-            .add(TAEntityTypes.FORGOTTEN_MAGIC_BOOK.get())
-            .add(TAEntityTypes.GIANT_CRYSTAL_SPIDER.get())
-            .add(TAEntityTypes.HYPHA_WALKING_MUSHROOM.get())
-            .add(TAEntityTypes.ICEFIELD_DEER.get())
-            .add(TAEntityTypes.MOON_ACOLYTE.get())
-            .add(TAEntityTypes.MOON_FISH.get())
-            .add(TAEntityTypes.MOON_QUEEN.get())
-            .add(TAEntityTypes.MOONLIGHT_KNIGHT.get())
-            .add(TAEntityTypes.ROCK_HAMMER.get())
-            .add(TAEntityTypes.RUNE_SPIDER.get())
-            .add(TAEntityTypes.RUNESTONE_KEEPER.get())
-            .add(TAEntityTypes.SELENA.get())
-            .add(TAEntityTypes.SNOW_TUNDRA_GIANT_CRAB.get())
-            .add(TAEntityTypes.SPIDER_MOTHER.get())
-            .add(TAEntityTypes.SPIDERLING.get())
-            .add(TAEntityTypes.SPIDERLING_CRYSTAL_SHELL.get())
-            .add(TAEntityTypes.SPIDERLING_WALL_CLIMBER.get())
-            .add(TAEntityTypes.SPIRIT.get())
-            .add(TAEntityTypes.STICKY_SPIKER.get())
-            .add(TAEntityTypes.TONG_SCORPION.get())
-            .add(TAEntityTypes.UNDEAD_KNIGHT.get());
     }
 
 }

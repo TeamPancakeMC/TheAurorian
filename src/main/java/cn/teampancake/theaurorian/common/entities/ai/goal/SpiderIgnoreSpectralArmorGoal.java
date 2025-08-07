d4cf31930646f24e67c2ecf80f874c7dcc579e87
glyceryl <494192175@qq.com>
2025-08-07 20:42:11 +0800

    修复蜘蛛会攻击其他生物的问题。

--- src/main/java/cn/teampancake/theaurorian/common/entities/ai/goal/SpiderIgnoreSpectralArmorGoal.java
+++ src/main/java/cn/teampancake/theaurorian/common/entities/ai/goal/SpiderIgnoreSpectralArmorGoal.java
@@ -5,6 +5,7 @@ import cn.teampancake.theaurorian.common.utils.TAInventoryUtils;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.player.Player;
 
 public class SpiderIgnoreSpectralArmorGoal<T extends LivingEntity> extends NearestAttackableTargetGoal<T> {
 
@@ -15,7 +16,7 @@ public class SpiderIgnoreSpectralArmorGoal<T extends LivingEntity> extends Neare
     @Override
     public boolean canUse() {
         boolean flag = false;
-        if (this.target != null) {
+        if (this.target != null && this.target instanceof Player) {
             flag = TAInventoryUtils.isWearFullArmor(this.target, SpectralArmor.class);
         }
 

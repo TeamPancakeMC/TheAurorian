79fa8908646a546a05e8f503fed3116cee0199f4
glyceryl <494192175@qq.com>
2025-09-25 22:11:32 +0800

    移除一个不需要的硬编码字符串参数。

--- src/main/java/cn/teampancake/theaurorian/client/gui/AurorianEventGui.java
+++ src/main/java/cn/teampancake/theaurorian/client/gui/AurorianEventGui.java
@@ -10,25 +10,23 @@ public final class AurorianEventGui {
 
     public static final int TEX_W = 512;
     public static final int TEX_H = 512;
-
     public static final int PAGE_W = 142;
     public static final int PAGE_H = 188;
 
     public enum Card {
-        COMBAT_NIGHT("战斗夜", 0,   0,   PAGE_W, PAGE_H),
-        PROTECTION_NIGHT("守护夜", 143, 0,   PAGE_W, PAGE_H),
-        EXPLORATION_NIGHT("探索夜", 285, 0,   PAGE_W, PAGE_H),
-        MINING_NIGHT("挖掘夜", 0,   189, PAGE_W, PAGE_H),
-        GROWTH_NIGHT("生长夜", 143, 189, PAGE_W, PAGE_H);
 
-        public final String displayName;
+        COMBAT_NIGHT(0, 0, PAGE_W, PAGE_H),
+        PROTECTION_NIGHT(143, 0, PAGE_W, PAGE_H),
+        EXPLORATION_NIGHT(285, 0, PAGE_W, PAGE_H),
+        MINING_NIGHT(0, 189, PAGE_W, PAGE_H),
+        GROWTH_NIGHT(143, 189, PAGE_W, PAGE_H);
+
         public final int u;
         public final int v;
         public final int width;
         public final int height;
 
-        Card(String displayName, int u, int v, int width, int height) {
-            this.displayName = displayName;
+        Card(int u, int v, int width, int height) {
             this.u = u;
             this.v = v;
             this.width = width;
@@ -38,6 +36,7 @@ public final class AurorianEventGui {
         public void blit(GuiGraphics gg, int dstX, int dstY) {
             gg.blit(TEXTURE, dstX, dstY, this.u, this.v, this.width, this.height, TEX_W, TEX_H);
         }
+
     }
 
     private AurorianEventGui() {}
